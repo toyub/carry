3a32b8176ba2a2565717d531ac659ffec1712e9f
姜河 <jianghe@icar99.com>
2015-01-30 14:04:15 +0800

    I18n, Timezone

---
+++ config/initializers/date_time_formaters.rb
@@ -0,0 +1,24 @@
+#date time formaters
+#defaults(activesupport/lib/active_support/core_ext/time/conversions.rb):
+#DATE_FORMATS = {
+#   :db           => '%Y-%m-%d %H:%M:%S',
+#   :number       => '%Y%m%d%H%M%S',
+#   :nsec         => '%Y%m%d%H%M%S%9N',
+#   :time         => '%H:%M',
+#   :short        => '%d %b %H:%M',
+#   :long         => '%B %d, %Y %H:%M',
+#   :long_ordinal => lambda { |time|
+#     day_format = ActiveSupport::Inflector.ordinalize(time.day)
+#     time.strftime("%B #{day_format}, %Y %H:%M")
+#   },
+#   :rfc822       => lambda { |time|
+#     offset_format = time.formatted_offset(false)
+#     time.strftime("%a, %d %b %Y %H:%M:%S #{offset_format}")
+#   },
+#   :iso8601      => lambda { |time| time.iso8601 }
+# }
+
+Time::DATE_FORMATS.merge!(
+  date_only: lambda {|time| I18n.l time, format: :date_only}
+  
+)
