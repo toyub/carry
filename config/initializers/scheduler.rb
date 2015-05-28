61974d03238030eb7c1ef282f62e5ace9199332b
issac <issaclau3@gmail.com>
2015-05-28 16:13:24 +0800

    fixbug

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,2 +1,6 @@
 require 'sidekiq/scheduler'
-Sidekiq.schedule = YAML.load_file(File.expand_path("../../../config/scheduler.yml",__FILE__))
+begin
+  Sidekiq.schedule = YAML.load_file(File.expand_path("../../../config/scheduler.yml",__FILE__))
+rescue NoMethodError => e
+  puts "scheduler文件为空，#{e.message}"
+end
