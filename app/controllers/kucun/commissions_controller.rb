33a8b620d37e49f2f423ecb7d79022b4f81448fd
姜河 <jianghe@icar99.com>
2015-07-14 15:30:50 +0800

    提成

--- app/controllers/kucun/commissions_controller.rb
+++ app/controllers/kucun/commissions_controller.rb
@@ -5,6 +5,7 @@ class Kucun::CommissionsController < Kucun::ControllerBase
   def show
     @store = current_user.store
     @store_material = @store.store_materials.find(params[:material_id])
+    
   end
 
 
--- app/controllers/kucun/trackings_controller.rb
+++ app/controllers/kucun/trackings_controller.rb
@@ -1,9 +1,16 @@
 class Kucun::TrackingsController < Kucun::ControllerBase
   def new
+    @store = current_user.store
+    @store_material = @store.store_materials.find(params[:material_id])
+    @tracking = StoreMaterialTracking.new
   end
 
   def show
     @store = current_user.store
     @store_material = @store.store_materials.find(params[:material_id])
+    @tracking = @store_material.store_material_tracking
+    if @tracking.blank?
+      redirect_to action: "new"
+    end
   end
 end
