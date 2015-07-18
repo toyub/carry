720b7cf531b24ef8dde5a9238f537706f0fd5701
issac <issaclau3@gmail.com>
2015-07-18 13:53:13 +0800

    去除重复

--- app/controllers/xiaoshou/service/profiles_controller.rb
+++ app/controllers/xiaoshou/service/profiles_controller.rb
@@ -6,12 +6,7 @@ module Xiaoshou
       end
 
       def new
-        @services = current_store.store_services
-        @categories = current_store.service_categories.order("created_at desc")
-        @material_categories = current_store.store_material_categories.super_categories.order("created_at desc")
-        @sub_categories = @material_categories.first.sub_categories.map {|c| [c.name, c.id]} if @material_categories.present?
-        category = @sub_categories.present? ? @sub_categories.first[1] : @material_categories.first.id
-        @materials = current_store.store_materials.where(store_material_category_id: category)
+        load_data
         @service = current_store.store_services.new
       end
 
@@ -20,17 +15,21 @@ module Xiaoshou
         if @service.save
           redirect_to [:new, :xiaoshou, :service, :setting]
         else
-          @services = current_store.store_services
-          @categories = current_store.service_categories.order("created_at desc")
-          @material_categories = current_store.store_material_categories.super_categories.order("created_at desc")
-          @sub_categories = @material_categories.first.sub_categories.map {|c| [c.name, c.id]} if @material_categories.present?
-          category = @sub_categories.present? ? @sub_categories.first[1] : @material_categories.first.id
-          @materials = current_store.store_materials.where(store_material_category_id: category)
+          load_data
           render :new
         end
       end
 
       private
+
+      def load_data
+        @services = current_store.store_services
+        @categories = current_store.service_categories.order("created_at desc")
+        @material_categories = current_store.store_material_categories.super_categories.order("created_at desc")
+        @sub_categories = @material_categories.first.sub_categories.map {|c| [c.name, c.id]} if @material_categories.present?
+        category = @sub_categories.present? ? @sub_categories.first[1] : @material_categories.first.id
+        @materials = current_store.store_materials.where(store_material_category_id: category)
+      end
 
       def profile_params
         params.require(:store_service).permit(
