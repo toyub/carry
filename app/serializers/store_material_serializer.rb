87481992fdb537ed299af55006ce27466affdbc2
姜河 <jianghe@icar99.com>
2015-08-13 13:31:38 +0800

    订货-新建订单

--- app/serializers/store_material_serializer.rb
+++ app/serializers/store_material_serializer.rb
@@ -1,7 +1,15 @@
 class StoreMaterialSerializer < ActiveModel::Serializer
-  attributes :id, :name, :unit, :barcode, :speci
+  attributes :id, :name, :unit, :barcode, :speci, :root_category, :category, :cost_price
 
   def unit
     object.store_material_unit.name
+  end
+
+  def root_category
+    object.store_material_root_category.name
+  end
+
+  def category
+    object.store_material_category.name
   end
 end
