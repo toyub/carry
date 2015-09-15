d575b5076a4284907071c2ba55b634d6e39d906e
姜河 <jianghe@icar99.com>
2015-09-15 17:25:05 +0800

    商品转移库

--- app/controllers/kucun/transfer/pickings_controller.rb
+++ app/controllers/kucun/transfer/pickings_controller.rb
@@ -10,6 +10,26 @@ module Kucun
       def create
         render json: params
       end
+
+      private
+      def picking_params
+        store_info = {
+              store_id: current_store.id,
+              store_chain_id: current_store.store_chain_id,
+              store_staff_id: current_user.id
+        }
+        safe_params = params.require(:picking).permit(:store_supplier_id, items_attributes: [
+            :store_material_id,
+            :store_material_inventory_id,
+            :store_depot_id,
+            :remark, :quantity
+          ])
+
+
+        safe_params.merge!(store_info)
+        safe_params[:items_attributes].each{|item| item.merge!(store_info)}
+        safe_params
+      end
     end
   end
 end
---
+++ app/models/store_material_picking.rb
@@ -0,0 +1,16 @@
+class StoreMaterialPicking < ActiveRecord::Base
+  belongs_to :sotre
+  belongs_to :store_chain
+  belongs_to :store_staff
+  belongs_to :store_supplier
+  has_many :items, class_name: 'StoreMaterialPickingItem'
+
+  accepts_nested_attributes_for :items
+
+  after_create :save_search_keys
+  private
+  def save_search_keys
+    self.search_keys = self.items.map(&->(item){item.store_material.name}).join(',');
+    self.save
+  end
+end
