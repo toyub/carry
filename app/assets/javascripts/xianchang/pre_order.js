dfd64ee35753ac28d7f305acc33ad5d406e5023c
mouseshi <linhongshimouse@gmail.com>
2015-09-24 16:25:58 +0800

    define Extjs order model

--- app/assets/javascripts/xianchang/pre_order.js
+++ app/assets/javascripts/xianchang/pre_order.js
@@ -6,88 +6,73 @@ Ext.require([
 ]);
 
 Ext.onReady(function(){
-    Ext.define('Book',{
-        extend: 'Ext.data.Model',
-        proxy: {
-            type: 'ajax',
-            reader: 'xml'
-        },
-        fields: [
-            // set up the fields mapping into the xml doc
-            // The first needs mapping, the others are very basic
-            {name: 'Author', mapping: '@author.name'},
-            'Title',
-            'Manufacturer',
-            'ProductGroup',
-            'DetailPageURL'
-        ]
-    });
 
-    // create the Data Store
-    var store = Ext.create('Ext.data.Store', {
-        model: 'Book',
-        proxy: {
-            // load using HTTP
-            type: 'ajax',
-            url: 'sheldon.xml',
-            // the return will be XML, so lets set up a reader
-            reader: {
-                type: 'xml',
-                record: 'Item',
-                totalProperty  : 'total'
-            }
-        }
-    });
+  Ext.define('Order',{
+      extend: 'Ext.data.Model',
+      proxy: {
+        type: 'ajax',
+        url: "/api/orders"
+      },
+      fields: [
+        { name: '编码', type: "string" },
+      ]
+  });
 
-    // create the grid
-    var grid = Ext.create('Ext.grid.Panel', {
-        bufferedRenderer: false,
-        store: store,
-        columns: [
-            {text: "Author", width: 120, dataIndex: 'Author', sortable: true},
-            {text: "Title", flex: 1, dataIndex: 'Title', sortable: true},
-            {text: "Manufacturer", width: 125, dataIndex: 'Manufacturer', sortable: true},
-            {text: "Product Group", width: 125, dataIndex: 'ProductGroup', sortable: true}
-        ],
-        forceFit: true,
-        height:210,
-        split: true,
-        region: 'north'
-    });
+  // create the Data Store
+  var store = Ext.create('Ext.data.Store', {
+    model: 'Order',
+  });
 
-    // define a template to use for the detail view
-    var bookTplMarkup = [
-        'Title: <a href="{DetailPageURL}" target="_blank">{Title}</a><br/>',
-        'Author: {Author}<br/>',
-        'Manufacturer: {Manufacturer}<br/>',
-        'Product Group: {ProductGroup}<br/>'
-    ];
-    var bookTpl = Ext.create('Ext.Template', bookTplMarkup);
+  // create the grid
+  var grid = Ext.create('Ext.grid.Panel', {
+    bufferedRenderer: false,
+    store: store,
+    columns: [
+        {xtype: 'rownumberer'},
+        {text: "Author", width: 120, dataIndex: 'Author', sortable: true},
+        {text: "Title", flex: 1, dataIndex: 'Title', sortable: true},
+        {text: "Manufacturer", width: 125, dataIndex: 'Manufacturer', sortable: true},
+        {text: "Product Group", width: 125, dataIndex: 'ProductGroup', sortable: true}
+    ],
+    forceFit: true,
+    height:210,
+    split: true,
+    region: 'north'
+  });
 
-    Ext.create('Ext.Panel', {
-        renderTo: 'pre-order-main',
-        frame: true,
-        title: 'Book List',
-        width: "100%",
-        height: 400,
-        layout: 'border',
-        items: [
-            grid, {
-                id: 'detailPanel',
-                region: 'center',
-                bodyPadding: 7,
-                bodyStyle: "background: #ffffff;",
-                html: 'Please select a book to see additional details.'
-        }]
-    });
+  // define a template to use for the detail view
+  var bookTplMarkup = [
+    'Title: <a href="{DetailPageURL}" target="_blank">{Title}</a><br/>',
+    'Author: {Author}<br/>',
+    'Manufacturer: {Manufacturer}<br/>',
+    'Product Group: {ProductGroup}<br/>'
+  ];
+  var bookTpl = Ext.create('Ext.Template', bookTplMarkup);
 
-    // update panel body on selection change
-    grid.getSelectionModel().on('selectionchange', function(sm, selectedRecord) {
-        if (selectedRecord.length) {
-            var detailPanel = Ext.getCmp('detailPanel');
-            detailPanel.update(bookTpl.apply(selectedRecord[0].data));
-        }
-    });
+  Ext.create('Ext.Panel', {
+    renderTo: 'pre-order-main',
+    frame: true,
+    title: '订单列表',
+    width: "100%",
+    height: 400,
+    layout: 'border',
+    items: [
+      grid, {
+        id: 'detailPanel',
+        region: 'center',
+        bodyPadding: 7,
+        bodyStyle: "background: #ffffff;",
+        html: '请添加Book'
+    }]
+  });
 
-    store.load();
+  // update panel body on selection change
+  grid.getSelectionModel().on('selectionchange', function(sm, selectedRecord) {
+    if (selectedRecord.length) {
+      var detailPanel = Ext.getCmp('detailPanel');
+      detailPanel.update(bookTpl.apply(selectedRecord[0].data));
+    }
+  });
+
+  store.load();
 });
