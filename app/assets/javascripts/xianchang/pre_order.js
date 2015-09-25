19ac9a6652f7e82fe7cf71e1acbf635ac8c2d057
mouseshi <linhongshimouse@gmail.com>
2015-09-25 15:55:37 +0800

    add Grid Panel bbar pagingtoolbar

--- app/assets/javascripts/xianchang/pre_order.js
+++ app/assets/javascripts/xianchang/pre_order.js
@@ -1,43 +1,49 @@
-Ext.require([
-    'Ext.grid.*',
-    'Ext.data.*',
-    'Ext.panel.*',
-    'Ext.layout.container.Border'
-]);
-
 Ext.onReady(function(){
 
-  Ext.define('Order',{
-      extend: 'Ext.data.Model',
-      proxy: {
-        type: 'ajax',
-        url: "/api/orders"
-      },
-      fields: [
-        { name: '编码', type: "string" },
-      ]
+  //Ext.define("StoreCustomer", {
+    //extend: "Ext.data.Model",
+    //fields: ["id", "full_name", "first_name"]
+  //});
+
+  Ext.define('StoreOrder',{
+    extend: 'Ext.data.Model',
+    fields: [
+      "id",
+      {name: "fullname", mapping: "store_customer.full_name" },
+    ]
   });
 
   // create the Data Store
   var store = Ext.create('Ext.data.Store', {
-    model: 'Order',
+    model: 'StoreOrder',
+    proxy: {
+      type: 'ajax',
+      url: "/api/store_orders"
+    },
   });
 
   // create the grid
-  var grid = Ext.create('Ext.grid.Panel', {
-    bufferedRenderer: false,
+  var orderGrid = Ext.create('Ext.grid.Panel', {
     store: store,
+    columnLines: true,
     columns: [
-        {xtype: 'rownumberer'},
-        {text: "Author", width: 120, dataIndex: 'Author', sortable: true},
-        {text: "Title", flex: 1, dataIndex: 'Title', sortable: true},
-        {text: "Manufacturer", width: 125, dataIndex: 'Manufacturer', sortable: true},
-        {text: "Product Group", width: 125, dataIndex: 'ProductGroup', sortable: true}
+      {xtype: 'rownumberer'},
+      {text: "编号", dataIndex: 'id', sortable: true},
+      {text: "预约人", dataIndex: 'fullname'},
+      {text: "车牌", dataIndex: 'store_customer_first_name'},
+      {text: "联系电话", dataIndex: 'store_customer_first_name'},
+      {text: "约定时间", dataIndex: 'store_customer_first_name'},
+      {text: "约定项目", dataIndex: 'store_customer_first_name'},
+      {text: "制单", dataIndex: 'store_customer_first_name'},
+      {text: "状态", dataIndex: 'store_customer_first_name'},
+      {text: "备注", dataIndex: 'store_customer_first_name'},
+      {text: "操作", dataIndex: 'store_customer.first_name'},
     ],
     forceFit: true,
-    height:210,
+    layout: 'fit',
     split: true,
-    region: 'north'
+    region: 'north',
+    bbar: {xtype: "pagingtoolbar", store: store, displayInfo: true}
   });
 
   // define a template to use for the detail view
@@ -54,20 +60,12 @@ Ext.onReady(function(){
     frame: true,
     title: '订单列表',
     width: "100%",
-    height: 400,
-    layout: 'border',
-    items: [
-      grid, {
-        id: 'detailPanel',
-        region: 'center',
-        bodyPadding: 7,
-        bodyStyle: "background: #ffffff;",
-        html: '请添加Book'
-    }]
+    layout: 'fit',
+    items: [orderGrid]
   });
 
   // update panel body on selection change
-  grid.getSelectionModel().on('selectionchange', function(sm, selectedRecord) {
+  orderGrid.getSelectionModel().on('selectionchange', function(sm, selectedRecord) {
     if (selectedRecord.length) {
       var detailPanel = Ext.getCmp('detailPanel');
       detailPanel.update(bookTpl.apply(selectedRecord[0].data));
