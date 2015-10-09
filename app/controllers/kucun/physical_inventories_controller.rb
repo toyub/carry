a1b99b8ba9b62ca848d889b35d64be78bd987042
姜河 <jianghe@icar99.com>
2015-10-09 17:04:12 +0800

    盘点

--- app/controllers/kucun/physical_inventories_controller.rb
+++ app/controllers/kucun/physical_inventories_controller.rb
@@ -5,10 +5,21 @@ class Kucun::PhysicalInventoriesController < Kucun::ControllerBase
 
   def new
     @store = current_store
+    
+    now = Time.now
+    month = now.strftime('%Y%m')
+
     if params[:store_depot_id].present?
+      @physical = StorePhysicalInventory.where(store_depot_id: @store.id).where(created_month: month).first
+      if @physical
+        ids = @physical.items.map {|item| item.store_inventory_id}
+      else
+        ids = []
+      end
       @inventories = StoreMaterialInventory.includes(:store_material)
                                            .where(store_id: @store.id,
                                                   store_depot_id: params[:store_depot_id])
+                                           .where('store_material_inventories.id not in (?)', ids)
       @inventories = @inventories.where('quantity > 0') if params[:nz]
     else
       @inventories = []
@@ -22,16 +33,25 @@ class Kucun::PhysicalInventoriesController < Kucun::ControllerBase
     end
     
     @physical = StorePhysicalInventory.new(physical_params)
-    render json: {
-      p: @physical,
-      items: @physical.items
-    }
+    @physical.store_staff_id = current_user.id
+    @physical.items.each do |item|
+      item.store_staff_id = @physical.store_staff_id
+      item.status = 1 if item.inventory == item.physical
+    end
+    @physical.save!
+    redirect_to edit_kucun_physical_inventory_path(@physical)
+  end
+
+  def edit
+    @store = current_store
+    @physical = StorePhysicalInventory.find(params[:id])
   end
 
   private
   def physical_params
     params.require(:store_physical_inventories)
           .permit(:store_depot_id, 
-                  items_attributes:[:store_material_id, :store_depot_id, :store_inventory_id, :inventory, :physical, :remark])
+                  items_attributes:[:store_material_id, :store_depot_id, :store_inventory_id,
+                                    :inventory, :physical, :inventory_cost_price, :cost_price, :remark])
   end
 end
--- app/models/store_physical_inventory.rb
+++ app/models/store_physical_inventory.rb
@@ -2,5 +2,14 @@ class StorePhysicalInventory < ActiveRecord::Base
   include BaseModel
   has_many :items, class_name: 'StorePhysicalInventoryItem'
 
+  before_save :set_created_month
+
   accepts_nested_attributes_for :items
+
+  private
+  def set_created_month
+    if self.new_record?
+      self.created_month = Time.now.strftime("%Y%m")
+    end
+  end
 end
