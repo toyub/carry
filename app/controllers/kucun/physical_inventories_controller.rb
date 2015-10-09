0a406f5f730932e617b790bcc7a9aace0d9a262e
姜河 <jianghe@icar99.com>
2015-10-09 13:36:06 +0800

    type='number'修改成自己写的插件

--- app/controllers/kucun/physical_inventories_controller.rb
+++ app/controllers/kucun/physical_inventories_controller.rb
@@ -4,9 +4,18 @@ class Kucun::PhysicalInventoriesController < Kucun::ControllerBase
   end
 
   def new
-    
+    @store = current_store
+    if params[:store_depot_id].present?
+      @inventories = StoreMaterialInventory.includes(:store_material)
+                                           .where(store_id: @store.id,
+                                                  store_depot_id: params[:store_depot_id])
+      @inventories = @inventories.where('quantity > 0') if params[:nz]
+    else
+      @inventories = []
+    end
   end
 
   def create
+    render json: params
   end
 end
