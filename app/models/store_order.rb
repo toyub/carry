41455934b6bcb9356bd9d7b5700919d686e3d1e6
mouseshi <linhongshimouse@gmail.com>
2015-10-10 10:33:07 +0800

    modify store order state val name add subscribe_order relationship with other model

--- app/models/store_order.rb
+++ app/models/store_order.rb
@@ -5,5 +5,5 @@ class StoreOrder < ActiveRecord::Base
   belongs_to :creator, class_name: "StoreStaff", foreign_key: :store_staff_id
   belongs_to :store_vehicle
 
-  enum state: %i[pending constructed waiting_pay paid]
+  enum state: %i[pending processing waiting_pay paid]
 end
