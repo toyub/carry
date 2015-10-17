f2d060533794529dcfbcb33261a6a35894a84803
mouseshi <linhongshimouse@gmail.com>
2015-10-17 11:04:46 +0800

    add javascript_include_tag helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,13 @@ module ApplicationHelper
     utn=Time.now
     return "#{type}#{utn.strftime('%Y%m%d%H%M%S')}#{((utn.to_f - utn.to_i) * 100_0000).to_i.to_s(36).rjust(5, '0').upcase}"
   end
+
+  def include_javascript_by_namespace
+    namespace = params[:controller].split('/')[0]
+    if ["kucun", "settings", "soa", "xianchang", "xiaoshou"].include?(params[:controller].split('/')[0])
+      content_for :pre_assets do
+        javascript_include_tag namespace
+      end
+    end
+  end
 end
