5101255ea664bd62e00ff37430daf3e26ba3fba6
姜河 <jianghe@icar99.com>
2015-10-17 13:35:15 +0800

    提成设置

--- app/controllers/settings/commission_templates_controller.rb
+++ app/controllers/settings/commission_templates_controller.rb
@@ -1,3 +1,32 @@
 class Settings::CommissionTemplatesController < Settings::BaseController
-  
+  def index
+
+    @templates = [
+      {
+        name: 'dslfkj',
+        mode_id: 0,
+        confined_to: 0,
+        status: 0,
+        sections: [
+          {
+            mode_id: 0,
+            type_id: 0,
+            source_id: 0,
+            amount: 2.2
+          }
+        ]
+
+      }
+
+    ]
+
+
+    respond_to do |format|
+      format.json {
+        render json: @templates
+      }
+
+      format.html {}
+    end
+  end
 end
