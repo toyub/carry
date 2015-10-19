119edb77e71cb0f7ce4c641ec71c6adbbc3cac96
姜河 <jianghe@icar99.com>
2015-10-19 11:45:57 +0800

    提成方案

--- app/controllers/settings/commission_templates_controller.rb
+++ app/controllers/settings/commission_templates_controller.rb
@@ -1,15 +1,20 @@
 class Settings::CommissionTemplatesController < Settings::BaseController
   def index
 
-    @templates = [
+    @templates = 15.times.map do |i|
+      mode_id = rand(3)
+      aim_to = rand(2)
+      confined_to = rand(1)
+
       {
-        name: 'dslfkj',
-        mode_id: 0,
-        confined_to: 0,
+        name: 'dslfkj'+i.to_s,
+        mode_id: mode_id,
+        aim_to: aim_to,
+        confined_to: confined_to,
         status: 0,
         sections: [
           {
-            mode_id: 0,
+            mode_id: mode_id,
             type_id: 0,
             source_id: 0,
             amount: 2.2
@@ -17,13 +22,12 @@ class Settings::CommissionTemplatesController < Settings::BaseController
         ]
 
       }
-
-    ]
+    end
 
 
     respond_to do |format|
       format.json {
-        render json: @templates
+        render json: @templates, root: false
       }
 
       format.html {}
