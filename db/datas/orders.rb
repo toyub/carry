ca68e9cb2ccf12313253bacfccd191ff394cf270
mouseshi <linhongshimouse@gmail.com>
2015-10-24 10:35:13 +0800

    use html5 date input

--- db/datas/orders.rb
+++ db/datas/orders.rb
@@ -2,13 +2,25 @@ puts "Now creating StoreSubscribeOrder..."
 
 StoreSubscribeOrder.delete_all
 
-StoreSubscribeOrder.create(
-  store_chain_id: StoreChain.first.try(:id),
-  store_staff_id: StoreStaff.first.try(:id),
-  remark: "要是忘记了，请提醒下我，谢谢",
-  store_customer: StoreCustomer.first,
-  store_vehicle: StoreVehicle.first,
-  subscribe_date: Time.now.end_of_month,
-  order_type: :auto,
+StoreSubscribeOrder.create([
+  {
+    store_chain_id: StoreChain.first.try(:id),
+    store_staff_id: StoreStaff.first.try(:id),
+    remark: "要是忘记了，请提醒下我，谢谢",
+    store_customer: StoreCustomer.first,
+    store_vehicle: StoreVehicle.first,
+    subscribe_date: Time.now.end_of_month,
+    order_type: :auto,
     state: :pending,
-)
+  },
+  {
+    store_chain_id: StoreChain.first.try(:id),
+    store_staff_id: StoreStaff.first.try(:id),
+    remark: "第二个预约订单",
+    store_customer: StoreCustomer.first,
+    store_vehicle: StoreVehicle.first,
+    subscribe_date: Time.now.end_of_month,
+    order_type: :auto,
+    state: :pending,
+  }
+])
