ae1cef2f2e1f4b0d7afd808a8a92931c37b8c8ba
姜河 <jianghe@icar99.com>
2015-10-28 14:59:02 +0800

    商品提成设置

--- app/serializers/store_commission_template_section_serializer.rb
+++ app/serializers/store_commission_template_section_serializer.rb
@@ -1,3 +1,5 @@
 class StoreCommissionTemplateSectionSerializer < ActiveModel::Serializer
-  attributes :mode_id, :type_id, :source_id, :amount
+  attributes :id, :store_id, :store_chain_id, :store_staff_id, :store_commission_template_id,
+             :mode_id, :mode_type, :type_id, :method_type, :source_id, :source_type,
+             :min, :max, :amount, :created_at, :updated_at
 end
