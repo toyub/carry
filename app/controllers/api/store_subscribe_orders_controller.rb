db125e4f3bed301996aa21c40063aec835b78f73
mouseshi <linhongshimouse@gmail.com>
2015-11-02 08:00:00 +0800

    subscribe order support phone number and subscribe date searching

--- app/controllers/api/store_subscribe_orders_controller.rb
+++ app/controllers/api/store_subscribe_orders_controller.rb
@@ -4,7 +4,12 @@ module Api
     protect_from_forgery except: :destroy
 
     def index
-      @orders = StoreSubscribeOrder.joins(:store_customer).where("store_customers.phone_number like ?", "%#{params[:phone]}")
+      @orders = if params[:number_val].present?
+                  StoreSubscribeOrder.joins(:store_customer).
+                    where("store_customers.phone_number like ?", "%#{params[:number_val]}%")
+                else
+                  StoreSubscribeOrder.all
+                end
       @orders = @orders.where(subscribe_date: params[:subscribe_date]) if params[:subscribe_date].present?
       @orders = @orders.where(state: params[:state]) if params[:state].present?
 
