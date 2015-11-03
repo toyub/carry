a1f4d44cfede0eb5c2e4fff4610ec8c439e9d21a
姜河 <jianghe@icar99.com>
2015-11-03 09:55:38 +0800

    商品销售设置

--- app/controllers/kucun/saleinfo_services_controller.rb
+++ app/controllers/kucun/saleinfo_services_controller.rb
@@ -1,7 +1,10 @@
 class Kucun::SaleinfoServicesController < Kucun::ControllerBase
-   def index
+  def index
     store_material = StoreMaterial.find(params[:material_id])
-    
-    render json: store_material.store_material_saleinfo.services, root: nil
-   end
+    if store_material.store_material_saleinfo.present?
+      render json: store_material.store_material_saleinfo.services, root: nil
+    else
+      render json: [], root: nil
+    end
+  end
 end
