e331748a3354b590c81f02ccdfa1f77ffe664dca
姜河 <jianghe@icar99.com>
2015-11-04 14:32:28 +0800

    商品回访设置

--- app/controllers/kucun/trackings_controller.rb
+++ app/controllers/kucun/trackings_controller.rb
@@ -1,14 +1,48 @@
 class Kucun::TrackingsController < Kucun::ControllerBase
   def create
-    tracking = StoreMaterialTracking.new(tracking_params)
+    store = current_store
+    store_material = store.store_materials.find(params[:material_id])
+
+    tracking = store_material.build_store_material_tracking(tracking_params)
+    tracking.store_staff_id = current_user.id
+    tracking.sections.each do |section|
+      section.store_staff_id = tracking.store_staff_id
+      section.store_material_id = tracking.store_material_id
+    end
+    tracking.save!
     render json: tracking, root: false
   end
 
   def update
+    store = current_store
+    store_material = store.store_materials.find(params[:material_id])
+    tracking = store_material.store_material_tracking
+    safe_params = tracking_params
+    if tracking.present?
+      if safe_params[:sections_attributes].present?
+        safe_params[:sections_attributes].each do |section|
+          if section[:id].blank?
+            section.merge!(tracking.attributes.symbolize_keys.slice(:store_material_id, :store_id, :store_chain_id))
+            section[:store_staff_id] = current_user.id
+          end
+        end
+      end
+      tracking.update!(safe_params)
+      render json: tracking, root: nil
+    else
+      render json: {msg: 'store material tracking not found, use create!'}
+    end
   end
 
   def sections
-    render json: [], root: nil
+    store = current_store
+    store_material = store.store_materials.find(params[:material_id])
+    stracking = store_material.store_material_tracking
+    if stracking.present?
+      render json: stracking.sections, root: nil
+    else
+      render json: [], root: nil
+    end
   end
 
   def show
@@ -19,7 +53,9 @@ class Kucun::TrackingsController < Kucun::ControllerBase
 
   private
   def tracking_params
-    params.require(:tracking).permit(:enabled, :tracking_mode, :reminder_required,
-                                     sections_attributes: [:timing, :delay_interval, :delay_unit, :delay_in_seconds, :contact_way, :content])
+    params.require(:tracking).permit(:id, :enabled, :tracking_mode, :reminder_required,
+                                     sections_attributes: [:id, :store_id, :store_chain_id, :store_staff_id, :store_material_id,
+                                                           :store_material_tracking_id, :timing, :delay_interval, :delay_unit,
+                                                           :delay_in_seconds, :contact_way, :content])
   end
 end
