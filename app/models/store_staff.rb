e8a6d3f153dd97134e1965a8a22fef4f58bcd010
lucidstory <lucidstory@sina.com>
2015-11-04 22:02:46 +0800

    staff_test

--- app/models/store_staff.rb
+++ app/models/store_staff.rb
@@ -1,5 +1,5 @@
 class StoreStaff <  ActiveRecord::Base
-  attr_accessor :password, :password_confirmation
+  attr_accessor :password, :password_confirmation, :phone_number
   belongs_to :store
   belongs_to :store_chain
 
