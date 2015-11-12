e9392f23cf8c8477b1d1f8470674b7119d09f628
姜河 <jianghe@icar99.com>
2015-11-12 15:26:25 +0800

    设置-仓库-新建仓库

--- app/controllers/settings/depots_controller.rb
+++ app/controllers/settings/depots_controller.rb
@@ -1,40 +1,64 @@
 class Settings::DepotsController < Settings::BaseController
+  before_action :set_store
+  before_action :set_depot, only: [:toggle_useable, :prefer, :binding_material_count, :destroy, :update]
+
   def index
-    @store = current_store
     @store_staff = @store.store_staff #loginable
   end
 
   def fetch
-    @store = current_store
-    @depots = @store.store_depots
+    @depots = @store.store_depots.where(deleted: false)
     render json: @depots, root: nil
   end
 
   def create
-    store = current_store
-
-    depot = store.store_depots.new(depot_params)
+    depot = @store.store_depots.new(depot_params)
     depot.save
     render json: depot, root: nil
   end
 
   def update
-    render json: depot_params, root: nil
+    @depot.update!(depot_params)
+    render json: @depot, root: nil
   end
 
   def toggle_useable
-
+    @depot.toggle_useable!
+    render json: {text: 'ok'}
   end
 
   def prefer
-
+    current_depot = @store.store_depots.current_preferred
+    if current_depot.present?
+      current_depot.update!(preferred: false)
+    end
+    @depot.update!(preferred: true)
+    render json: {text: 'ok'}
   end
 
   def binding_material_count
-    render json: {bound: 12}
+    count = @depot.binding_material_count
+    render json: {bound: count}
+  end
+
+  def destroy
+    if @depot.useable
+      render status: 409, json: {error: '不能删除正在使用的仓库!'}
+    else
+      @depot.update!(deleted: true)
+      render json: {text: 'ok'}
+    end
   end
 
   private
+  def set_store
+    @store = current_store
+  end
+
+  def set_depot
+    @depot = @store.store_depots.find(params[:id])
+  end
+
   def depot_params
     params.require(:depot).permit(:id, :name, :description, admin_ids: []).merge(store_staff_id: current_staff.id)
   end
--- app/models/store_depot.rb
+++ app/models/store_depot.rb
@@ -1,4 +1,16 @@
 class StoreDepot  < ActiveRecord::Base
   include BaseModel
   has_many :store_material_inventories
+  
+  def toggle_useable!
+    self.update!(useable: !self.useable)
+  end
+
+  def self.current_preferred
+    self.where(preferred: true).first
+  end
+
+  def binding_material_count
+    self.store_material_inventories.count(:id)
+  end
 end
