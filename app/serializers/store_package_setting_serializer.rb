2dcbb7760b72cc9eb08f4ec8c9d834edb01705c0
issac <issaclau3@gmail.com>
2015-11-13 14:36:42 +0800

    tidy serializer

--- app/serializers/store_package_setting_serializer.rb
+++ app/serializers/store_package_setting_serializer.rb
@@ -1,3 +1,16 @@
 class StorePackageSettingSerializer < ActiveModel::Serializer
-  attributes :id, :retail_price, :period_enable, :period_unit, :period, :expired_notice_required, :before_expired, :apply_range, :point, :payment_mode, :store_commission_template_id
+  FILEDS = [
+    :id,
+    :retail_price,
+    :period_enable,
+    :period_unit,
+    :period,
+    :expired_notice_required,
+    :before_expired,
+    :apply_range,
+    :point,
+    :payment_mode,
+    :store_commission_template_id]
+
+  attributes *FILEDS
 end
