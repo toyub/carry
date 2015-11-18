5177dcfe5517c651c9d3f5c90d0f63cf1032c44d
姜河 <jianghe@icar99.com>
2015-11-18 11:28:28 +0800

    商品类别

--- app/models/store_material_category.rb
+++ app/models/store_material_category.rb
@@ -1,10 +1,11 @@
 class StoreMaterialCategory < ActiveRecord::Base
-  belongs_to :store
-  belongs_to :store_chain
+  include BaseModel
   belongs_to :parent, class_name: 'StoreMaterialCategory', foreign_key: 'parent_id'
   has_many :sub_categories, class_name: 'StoreMaterialCategory', foreign_key: 'parent_id'
 
   scope :super_categories, ->(){where parent_id: 0}
+
+  validates_presence_of :name
 
   def has_parent?
     self.parent_id.present? && self.parent_id > 0
