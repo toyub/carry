8aeec9ea9a98df42aaa0e6498531042b122d37c8
姜河 <jianghe@icar99.com>
2015-11-20 10:46:40 +0800

    store departments

--- app/controllers/api/store_departments_controller.rb
+++ app/controllers/api/store_departments_controller.rb
@@ -3,6 +3,7 @@ module Api
     def index
       departments = [
         {
+          id: 12,
           name: 'xxxxxx',
           departments: [
             {
@@ -27,6 +28,19 @@ module Api
       ]
 
       respond_with departments, location: nil
+    end
+
+    def create
+      render json: department_params
+    end
+
+    def update
+      render json: department_params
+    end
+
+    private
+    def department_params
+      params.require(:store_department).permit(:parent_id, :name)
     end
   end
 end
