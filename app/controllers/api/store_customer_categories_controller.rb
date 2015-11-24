008a3dbc00008e1ff25aa023c488649dd0ab99ad
姜河 <jianghe@icar99.com>
2015-11-24 17:13:23 +0800

    商品类别

--- app/controllers/api/store_customer_categories_controller.rb
+++ app/controllers/api/store_customer_categories_controller.rb
@@ -1,6 +1,6 @@
 class Api::StoreCustomerCategoriesController < Api::BaseController
   def index
-    
+    render json: '{"name":"sdafsdaf","color":"#000000","description":"sdfsdafsdf sadfsdaf","conditions":{"consume_amount":"2323","cards_count":"23","orders_count":"323","profile_integrity_percentage":"43"},"auto_promoted_enabled":"on","discounts":{"material_root_categories":[{"id":"1","forall_enabled":"on","forall_rate":"23","sub_categories":[{"id":"2"}]},{"id":"3","sub_categories":[{"id":"4","rate":"23"}]},{"id":"5","sub_categories":[{"id":"6","rate":"32"},{"id":"12","rate":"32"}]},{"id":"7","forall_enabled":"on","forall_rate":"23","sub_categories":[{"id":"8"},{"id":"9"},{"id":"10"},{"id":"11"}]},{"forall_enabled":false},{"forall_enabled":false}],"service_root_categories":[{"id":"2","services":[{"id":"2","rate":"32"}]},{"id":"1","forall_enabled":"on","forall_rate":"23","services":[{"id":"1"}]},{"forall_enabled":false}]},"privileges":{"subscribe":"on","queue":"on","additional_discount":"on"}}'
   end
 
   def create
