648ca3f1055a69a27248ae4f702cb1af74f67f40
姜河 <jianghe@icar99.com>
2015-11-25 16:51:17 +0800

    客户类别

--- app/controllers/api/store_customer_categories_controller.rb
+++ app/controllers/api/store_customer_categories_controller.rb
@@ -1,14 +1,17 @@
 class Api::StoreCustomerCategoriesController < Api::BaseController
   def index
-    render json: '[{"name":"Psdfa Cf","color":"#c50f40","description":"sdfsdafsdf sadfsdaf","conditions":{"consume_amount":2323,"cards_count":23,"orders_count":323,"profile_integrity_percentage":43},"auto_promoted_enabled":"on","discounts":{"material_root_categories":{"1":{"id":1,"forall_enabled":"on","forall_rate":23,"sub_categories":{"2":{"id":2,"rate":""}}},"3":{"id":3,"forall_enabled":"on","forall_rate":23,"sub_categories":{"4":{"id":4,"rate":""}}},"5":{"id":5,"sub_categories":{"6":{"id":6,"rate":23},"12":{"id":12,"rate":32}},"forall_enabled":false},"7":{"id":7,"sub_categories":{"8":{"id":8,"rate":32},"9":{"id":9,"rate":23},"10":{"id":10,"rate":34},"11":{"id":11,"rate":34}},"forall_enabled":false}},"service_root_categories":{"1":{"id":1,"forall_enabled":"on","forall_rate":33,"services":{"1":{"id":1}}},"2":{"id":2,"services":{"2":{"id":2,"rate":33}},"forall_enabled":false}}},"privileges":{"subscribe":"on","queue":"on","additional_discount":"on"}}, {"name":"Psdfa Cf","color":"#c50f40","description":"sdfsdafsdf sadfsdaf","conditions":{"consume_amount":2323,"cards_count":23,"orders_count":323,"profile_integrity_percentage":43},"auto_promoted_enabled":"on","discounts":{"material_root_categories":{"1":{"id":1,"forall_enabled":"on","forall_rate":23,"sub_categories":{"2":{"id":2,"rate":""}}},"3":{"id":3,"forall_enabled":"on","forall_rate":23,"sub_categories":{"4":{"id":4,"rate":""}}},"5":{"id":5,"sub_categories":{"6":{"id":6,"rate":23},"12":{"id":12,"rate":32}},"forall_enabled":false},"7":{"id":7,"sub_categories":{"8":{"id":8,"rate":32},"9":{"id":9,"rate":23},"10":{"id":10,"rate":34},"11":{"id":11,"rate":34}},"forall_enabled":false}},"service_root_categories":{"1":{"id":1,"forall_enabled":"on","forall_rate":33,"services":{"1":{"id":1}}},"2":{"id":2,"services":{"2":{"id":2,"rate":33}},"forall_enabled":false}}},"privileges":{"subscribe":"on","queue":"on","additional_discount":"on"}}]'
+    respond_with current_store.store_customer_categories, location: nil
   end
 
   def create
-    render json: category_params
+    store_customer_category = current_store.store_customer_categories.create(append_store_attrs(category_params))
+    respond_with store_customer_category, location: nil
   end
 
   def update
-    render json: category_params
+    store_customer_category = current_store.store_customer_categories.find(params[:id])
+    store_customer_category.update(category_params)
+    respond_with store_customer_category, location: nil
   end
 
   private
