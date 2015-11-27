7770ce9815c4f056683138fef6210dcf9b619b3f
issac <issaclau3@gmail.com>
2015-11-27 16:05:39 +0800

    delete migration

--- db/migrate/20151126035810_change_range_type_of_store_customers.rb
+++ db/migrate/20151126035810_change_range_type_of_store_customers.rb
@@ -1,12 +1,12 @@
 class ChangeRangeTypeOfStoreCustomers < ActiveRecord::Migration
   def up
-    change_column :store_customers, :range, :string
-    change_column :store_customers, :education, :string
-    change_column :store_customers, :profession, :string
-    change_column :store_customers, :income, :string
-    change_column :store_customers, :settlement_mode, :string
-    change_column :store_customers, :payment_mode, :string
-    change_column :store_customers, :invoice_category, :string
+    remove_column :store_customers, :range, :string
+    remove_column :store_customers, :education, :string
+    remove_column :store_customers, :profession, :string
+    remove_column :store_customers, :income, :string
+    remove_column :store_customers, :settlement_mode, :string
+    remove_column :store_customers, :payment_mode, :string
+    remove_column :store_customers, :invoice_category, :string
   end
 
   def down
