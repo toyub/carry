827cf42b0866659458216c3f85e837782bc47f2a
姜河 <jianghe@icar99.com>
2015-12-01 14:12:10 +0800

    门店信息

--- app/controllers/settings/stores_controller.rb
+++ app/controllers/settings/stores_controller.rb
@@ -1,12 +1,12 @@
 class Settings::StoresController < Settings::BaseController
   def show
-    @open_time = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '上班时间').first
-    @close_time = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '下班时间').first
-    @state = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '省份').first
-    @city = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '城市').first
-    @address = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '详细地址').first
-    @longi_and_lati = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '经纬度').first
-    @contact_phone = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '联系电话').first
+    @open_time = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '上班时间').where(using: true).first
+    @close_time = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '下班时间').where(using: true).first
+    @state = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '省份').where(using: true).first
+    @city = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '城市').where(using: true).first
+    @address = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '详细地址').where(using: true).first
+    @longi_and_lati = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '经纬度').where(using: true).first
+    @contact_phone = current_store.store_infos.joins(:info_category).where('info_categories.name = :name', name: '联系电话').where(using: true).first
   end
 
   def edit
