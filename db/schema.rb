eb11045d73af93dcf7499ffda9a2ddf18364cdd7
朱士松 <zhushisong@icar99.com>
2015-12-08 09:31:25 +0800

    add-migration-for-agent

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208012032) do
+ActiveRecord::Schema.define(version: 20151208013001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151208012032) do
     t.text     "remark"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "creator_id"
   end
 
   create_table "ca_stations", force: :cascade do |t|
