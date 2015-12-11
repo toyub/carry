08823e91907f700f50456283f694fabadc904460
mouseshi <linhongshimouse@gmail.com>
2015-12-11 11:34:50 +0800

    set full name before_save

--- app/models/store_customer.rb
+++ app/models/store_customer.rb
@@ -2,6 +2,7 @@ class StoreCustomer < ActiveRecord::Base
   include BaseModel
 
   belongs_to :store_customer_entity
+  before_save :set_full_name
 
   def province
     self.district[:province]
@@ -14,4 +15,12 @@ class StoreCustomer < ActiveRecord::Base
   def region
     self.district[:region]
   end
+
+  private
+
+    def set_full_name
+      if first_name_changed? || last_name_changed?
+        self.full_name = first_name + last_name
+      end
+    end
 end
