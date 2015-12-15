820997a65869864ba91dbc3efae2ed51cd6b7566
姜河 <jianghe@icar99.com>
2015-12-15 13:29:58 +0800

    短信充值

--- app/controllers/open/topups/alipays_controller.rb
+++ app/controllers/open/topups/alipays_controller.rb
@@ -3,54 +3,36 @@ module Open
     before_action :check_params
 
     # transaction do
-    #    create_payment.then.create_debit.then.increase_balance
-    #    create_credit.then.decrease_balance.then.finish_order
+    #    create_or_update_payment.then.create_debit.then.increase_balance
+    #    create_or_update_order.create_credit.then.decrease_balance.then.finish_order
+    #    increase_sms_balance
     # end
 
     def return_url
-      if Alipay.valid_alipay_param?(alipay_params.except(:sign, :sign_type), alipay_params[:sign], alipay_params[:sign_type])
-        topup_transaction!(alipay_params)
-      end
+      order = Order.find_by_pretty_id(alipay_params[:out_trade_no])
+      payment = save_third_party_params(order, {return_url: alipay_params, return_at: Time.now})
+      topup_transaction!(order, payment)
     end
 
     def notify_url
-      if Alipay.valid_alipay_param?(alipay_params.except(:sign, :sign_type), alipay_params[:sign], alipay_params[:sign_type])
-        topup_transaction!(alipay_params)
-      end
+      order = Order.find_by_pretty_id(alipay_params[:out_trade_no])
+      payment = save_third_party_params(order, {notify_url: alipay_params, notify_at: Time.now})
+      topup_transaction!(order, payment)
       render text: 'success'
     end
 
     private
 
-    def topup_transaction!(pure_params)
+    def topup_transaction!(order, payment)
       ActiveRecord::Base.transaction do
-        order = Order.find_by_pretty_id(pure_params[:out_trade_no])
-        payment = save_notify_url(order, pure_params)
         unless order.paid
           create_debit(payment)
           create_credit(order)
+          increase_sms_balance(order)
           order.paid = true
           order.save
         end
       end
-    end
-
-    def alipay_params
-      params.except(:action, :controller)
-    end
-
-    def check_params
-      unless (params.has_key?(:sign) && params.has_key?(:out_trade_no) && params.has_key?(:trade_status))
-        render text: 'Some arguments has worg opinion!'
-      end
-    end
-
-    def save_return_url(order, options)
-      save_third_party_params(order, {return_url: options, return_at: Time.now})
-    end
-
-    def save_notify_url(order, options)
-      save_third_party_params(order, {notify_url: options, notify_at: Time.now})
     end
 
     def save_third_party_params(order, options)
@@ -67,6 +49,27 @@ module Open
 
     def create_debit(payment) #Debit has nothing share with order
       Journalable.create_debit!(payment)
+    end
+
+    def increase_sms_balance(order)
+      sms_balance = SmsBalance.find_or_create_by(party: order.party)
+      sms_balance.increase_total!(order.order_items.map(&->(item){item.quantity}).sum)
+    end
+
+    def alipay_params
+      params.except(:action, :controller)
+    end
+
+    def check_params
+      unless (params.has_key?(:sign) && params.has_key?(:out_trade_no) && params.has_key?(:trade_status))
+        render text: 'Some arguments has worg opinion!'
+        return false
+      end
+
+      unless Alipay.valid_alipay_param?(alipay_params.except(:sign, :sign_type), alipay_params[:sign], alipay_params[:sign_type])
+        render text: 'Some arguments has worg opinion!'
+        return false
+      end
     end
   end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ActiveRecord::Base
   belongs_to :party, polymorphic: true
-
+  has_one :debit
   validates :party_type, :party_id, presence: true
 end
