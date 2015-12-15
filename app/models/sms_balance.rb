803f39baf7b935bdba815d60266d380a7aa984ac
姜河 <jianghe@icar99.com>
2015-12-15 10:42:16 +0800

    创建短信余额

---
+++ app/models/sms_balance.rb
@@ -0,0 +1,16 @@
+class SmsBalance < ActiveRecord::Base
+  belongs_to :party, polymorphic: true
+  validates :party_type, :party_id, presence: true
+
+  def left_quantity
+    self.total-self.sent_quantity
+  end
+
+  def increase_total!(quantity)
+    self.class.unscoped.where(id: self.id).update_all("total=COALESCE(total, 0) + #{quantity.to_i.abs}")
+  end
+
+  def increase_sent_quantity!(quantity)
+    self.class.unscoped.where(id: self.id).update_all("sent_quantity=COALESCE(sent_quantity, 0) + #{quantity.to_i.abs}")
+  end
+end
