0d73cc268cfedd444d8ab9b5c6412f41c3459014
radiocontroller <zsh302643159@163.com>
2015-12-17 09:27:13 +0800

    车辆档案完成

--- app/services/create_store_vehicle_service.rb
+++ app/services/create_store_vehicle_service.rb
@@ -5,38 +5,33 @@ class CreateStoreVehicleService
     @vehicle = vehicle
     @license_number = license_number
     @identification_number = identification_number
+    @plate = StoreVehicleRegistrationPlate.find_by(license_number: @license_number)
+    @engine = StoreVehicleEngine.find_by(identification_number: @identification_number)
   end
 
   def call
     @vehicle.transaction do
       @vehicle.save!
-      if (plate = StoreVehicleRegistrationPlate.find_by(license_number: @license_number)).blank?
-        plate = StoreVehicleRegistrationPlate.new(license_number: @license_number)
-        plate.set_attrs({
-                          store_id: @vehicle.store_id,
-                          store_customer_id: @vehicle.store_customer_id,
-                          store_chain_id: @vehicle.store_chain_id,
-                          store_staff_id: @vehicle.store_staff_id
-                        })
-        plate.save!
-      end
-
-      if (engine = StoreVehicleEngine.find_by(identification_number: @identification_number)).blank?
-        engine = StoreVehicleEngine.new(identification_number: @identification_number)
-        engine.set_attrs({
-                          store_id: @vehicle.store_id,
-                          store_customer_id: @vehicle.store_customer_id,
-                          store_chain_id: @vehicle.store_chain_id,
-                          store_staff_id: @vehicle.store_staff_id
-                        })
-        engine.save!
-      end
-      @vehicle.plates << plate
-      @vehicle.engines << engine
+      @plate ||= StoreVehicleRegistrationPlate.create! plate_attr
+      @engine ||= StoreVehicleEngine.create! engine_attr
+      @vehicle.plates << @plate
+      @vehicle.engines << @engine
     end
     true
   rescue ActiveRecord::RecordInvalid
     false
+  end
+
+  def vehicle_attrs
+    @vehicle.attributes.symbolize_keys.slice(:store_id, :store_customer_id, :store_chain_id, :store_staff_id)
+  end
+
+  def plate_attr
+    vehicle_attrs.merge(license_number: @license_number)
+  end
+
+  def engine_attr
+    vehicle_attrs.merge(identification_number: @identification_number)
   end
 
 end
