e7c59654cea03ebb432f5ec141462b0bd1168581
姜河 <jianghe@icar99.com>
2015-12-18 08:55:56 +0800

    调整收银字段

--- app/models/mocks/item.rb
+++ app/models/mocks/item.rb
@@ -3,15 +3,15 @@ module Mocks
     def self.mock
       
       {
-              name: '3M Beat Oil',
-              speci: '1500ml',
-              unit: 'Bottle',
-              quantity: '1',
-              price: '350',
-              huiyuan_price: ['320','-'].sample,
-              cut: '2',
-              amount: '298'
-            }
+        name: '3M Beat Oil',
+        speci: '1500ml',
+        unit: 'Bottle',
+        quantity: '1',
+        price: '350',
+        huiyuan_price: ['320','-'].sample,
+        cut: '2',
+        amount: '298'
+      }
     end
   end
 end
