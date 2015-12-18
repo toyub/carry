fb457e1c5501f8c50e0dbebf6564a91028c0a530
radiocontroller <zsh302643159@163.com>
2015-12-18 16:27:32 +0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -235,6 +235,7 @@ Rails.application.routes.draw do
       resources :store_vehicle_status, only: [:show]
       resources :store_vehicle_service_records, only: [:show]
       resources :expense_records, only: [:index]
+      resources :pre_orders, only: [:index]
     end
   end
 
