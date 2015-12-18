1f70ef95daf268598bed9356a615be5ec3d21550
issac <issaclau3@gmail.com>
2015-12-18 20:01:58 +0800

    use store_chain query customers

--- app/controllers/erp/customers_controller.rb
+++ app/controllers/erp/customers_controller.rb
@@ -1,7 +1,7 @@
 module Erp
   class CustomersController < BaseController
     def index
-      @q = current_store.store_customers.ransack(params[:q])
+      @q = current_store_chain.store_customers.ransack(params[:q])
       @customers = @q.result.select("DISTINCT ON (store_customers.id) store_customers.*")
       respond_with @customers, location: nil
     end
