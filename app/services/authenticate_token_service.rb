a954129551e4ceed1c04f349f4dce6d25b6e3712
issac <issaclau3@gmail.com>
2015-12-18 13:33:22 +0800

    将token缓存起来

--- app/services/authenticate_token_service.rb
+++ app/services/authenticate_token_service.rb
@@ -9,8 +9,19 @@ class AuthenticateTokenService
   end
 
   def call
-    response = HttpClient.get(AUTH_URL + "/#{@key}/auth", {secret: @secret}, {})
-    return false if response.status != 200
-    JSON.parse(response.body)["success"]
+    Rails.cache.fetch cache_key do
+      response = HttpClient.get(path, {secret: @secret}, {})
+      return false if response.status != 200
+      JSON.parse(response.body)["success"]
+    end
+  end
+
+  private
+  def path
+    AUTH_URL + "/#{@key}/auth"
+  end
+
+  def cache_key
+    "app_auth_token:#{@key}:#{@secret}"
   end
 end
