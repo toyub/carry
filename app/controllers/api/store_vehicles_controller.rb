f6048afab7536f1e8a54277e77b2bc9ab333fc96
mouseshi <linhongshimouse@gmail.com>
2015-12-19 14:04:58 +0800

    store vehicle license_number searching

--- app/controllers/api/store_vehicles_controller.rb
+++ app/controllers/api/store_vehicles_controller.rb
@@ -6,5 +6,15 @@ module Api
 
       render json: @store_vehicles
     end
+
+    def search
+      @store_vehicle_registration_plates = StoreVehicleRegistrationPlate.where("license_number like ?", "%#{params[:q]}%")
+      store_vehicle_ids = @store_vehicle_registration_plates.pluck(:store_vehicle_id)
+
+      @store_vehicles = StoreVehicle.where(id: store_vehicle_ids)
+      results = @store_vehicles.map{ |store_vehicle| {id: store_vehicle.id, text: store_vehicle.registration_plate.license_number } }
+
+      render json: {items: results}
+    end
   end
 end
