ec8233046b113bd6ce59d15c0f5baefbb3c7a958
FaithTan <seventanyb@gmail.com>
2015-12-19 17:10:25 +0800

    remove to_i from regular?

--- app/models/store_staff.rb
+++ app/models/store_staff.rb
@@ -71,7 +71,7 @@ class StoreStaff <  ActiveRecord::Base
   end
 
   def working_age
-    Time.now.year - (employeed_at.try(:year) || created_at.try(:year)).to_i
+    Time.now.year - employeed_at.year || created_at.year
   end
 
   def insurence_enabled?
