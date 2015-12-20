407bd3c0f0a9bb47bb553965dc87310bacb96693
朱士松 <zhushisong@icar99.com>
2015-12-20 14:59:14 +0800

    优化投诉类别和方式的显示方式

--- app/controllers/crm/expense_records_controller.rb
+++ app/controllers/crm/expense_records_controller.rb
@@ -1,11 +1,17 @@
 class Crm::ExpenseRecordsController < Crm::BaseController
   before_action :set_customer
   def index
+    emu
     @q = @customer.orders.ransack(params[:q])
     @orders = @q.result.includes(:store_vehicle)
   end
 
   private
+  def emu
+    @complaint_categories = Complaint::CATEGORY
+    @complaint_ways = Complaint::WAY
+  end
+
   def set_customer
     @customer = StoreCustomer.find(params[:store_customer_id])
   end
