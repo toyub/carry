602ef2d5afc0eed4321faddb67b20a1634d80148
姜河 <jianghe@icar99.com>
2015-12-21 16:28:50 +0800

    付款

--- app/controllers/api/store_checkouts_controller.rb
+++ app/controllers/api/store_checkouts_controller.rb
@@ -2,8 +2,18 @@ module Api
   class StoreCheckoutsController < BaseController
 
     def create
-      
-      render json: params
+      order = Mocks::Order.find_by_id(params[:order_id])
+      if params[:payments].present?
+        save_payments(params[:payments])
+        Mocks::Order.checked!(params[:order_id])
+        render json: {checked:true, msg: 'Checked!'}
+      else
+        render json: {checked:true, msg: 'Payments required!'}
+      end
+    end
+
+    def save_payments(payments)
+
     end
 
   end
