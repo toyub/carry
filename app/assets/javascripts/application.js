4f6739448228d8998ad29d42537f84fe231bddd2
FaithTan <seventanyb@gmail.com>
2015-12-24 09:29:38 +0800

    将echarts改为单文件引入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,6 +36,7 @@
 //= require dialogs/piccut
 //= require dialogs/form
 //= require jquery_ext/jquery.datetimepicker
+//= require 'echarts/2.2.7'
 
 function ZhanchuangAlert(msg) {
   $.alert({text: msg})
