01b4fabf10cc19789f7aab943871d299b949790a
issac <issaclau3@gmail.com>
2015-12-24 12:26:38 +0800

    create model

---
+++ db/migrate/20151224041726_create_store_repayments.rb
@@ -0,0 +1,12 @@
+class CreateStoreRepayments < ActiveRecord::Migration
+  def change
+    create_table :store_repayments do |t|
+      t.decimal :amount
+      t.integer :store_id
+      t.integer :store_chain_id
+      t.integer :store_staff_id
+
+      t.timestamps null: false
+    end
+  end
+end
