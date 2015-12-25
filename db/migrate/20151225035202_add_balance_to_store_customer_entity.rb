7d54a44c3eb1a031bff278e0b400c7e1c77633ef
radiocontroller <zsh302643159@163.com>
2015-12-25 11:57:18 +0800

    commit

--- db/migrate/20151225035202_add_balance_to_store_customer_entity.rb
+++ db/migrate/20151225035202_add_balance_to_store_customer_entity.rb
@@ -1,5 +1,5 @@
 class AddBalanceToStoreCustomerEntity < ActiveRecord::Migration
   def change
-    add_column :store_customer_entities, :balance, :decimal
+    add_column :store_customer_entities, :balance, :decimal, default: 0.0, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -406,12 +406,12 @@ ActiveRecord::Schema.define(version: 20151225035202) do
     t.float    "range"
     t.string   "property"
     t.string   "remark"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.integer  "store_id"
     t.integer  "store_staff_id"
     t.integer  "store_chain_id"
-    t.decimal  "balance"
+    t.decimal  "balance",                    default: 0.0, null: false
   end
 
   create_table "store_customer_journal_entries", force: :cascade do |t|
