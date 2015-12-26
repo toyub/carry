656093eff5d4bb7e851c0f744e44677c527a8191
FaithTan <seventanyb@gmail.com>
2015-12-26 10:39:10 +0800

    remove unnecessary editors

--- app/controllers/api/sas/customer_gender_controller.rb
+++ app/controllers/api/sas/customer_gender_controller.rb
@@ -1,11 +1,8 @@
 class Api::Sas::CustomerGenderController < Api::BaseController
   def index
     @data = {
-      title: ['女性','男性'],
-      content: { category: ['会员客户','非会员客户','集团客户'],
-                 female: [120, 132, 101],
-                 male: [220, 182, 191],
-    }
+      female: [120, 132, 101],
+      male: [220, 182, 191],
     }
     render json: @data
   end
