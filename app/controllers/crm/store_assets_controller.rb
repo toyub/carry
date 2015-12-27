324bda7e2f81d0c5af60cd0342b7f7660fcd6096
FaithTan <seventanyb@gmail.com>
2015-12-27 16:03:09 +0800

    查看功能

--- app/controllers/crm/store_assets_controller.rb
+++ app/controllers/crm/store_assets_controller.rb
@@ -2,4 +2,13 @@ class Crm::StoreAssetsController < Crm::BaseController
   def index
     @customer = StoreCustomer.find(params[:store_customer_id])
   end
+
+  def show
+    @customer = StoreCustomer.find(params[:store_customer_id])
+    @patial = "packages_consume_history"
+
+    respond_to do |format|
+      format.js
+    end
+  end
 end
