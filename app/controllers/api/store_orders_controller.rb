49bfe402c1cd0b13d3645eba49ef7d8252035c67
mouseshi <linhongshimouse@gmail.com>
2015-12-29 07:06:07 +0800

    save store_order store_package itemable

--- app/controllers/api/store_orders_controller.rb
+++ app/controllers/api/store_orders_controller.rb
@@ -13,11 +13,27 @@ module Api
 
     def create
       items_attributes = []
+
       if params[:material_saleinfos]
         items_attributes = params[:material_saleinfos].values.map do |info|
           {
             orderable_id: info["id"],
             orderable_type: "StoreMaterialSaleinfo",
+            vip_price: info["vip_price"],
+            quantity: info["quantity"],
+            price: info["retail_price"],
+            # TODO 这个价格需要被修改
+            amount: info["quantity"].to_f * info["retail_price"].to_f,
+            creator: current_staff,
+          }
+        end
+      end
+
+      if params[:packages]
+        items_attributes += params[:packages].values.map do |info|
+          {
+            orderable_id: info["id"],
+            orderable_type: "StorePackage",
             vip_price: info["vip_price"],
             quantity: info["quantity"],
             price: info["retail_price"],
