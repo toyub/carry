a72c6d79ef7a5fd71a4ba0ea0556c1b418ca9db1
FaithTan <seventanyb@gmail.com>
2015-12-28 17:31:55 +0800

    assetitem详情查看js)+ing

--- app/models/store_customer_asset_item.rb
+++ app/models/store_customer_asset_item.rb
@@ -1,8 +1,9 @@
 class StoreCustomerAssetItem < ActiveRecord::Base
   belongs_to :assetable, polymorphic: true
   belongs_to :store_customer
+  has_many :store_customer_asset_logs
 
   def logs
-    StoreCustomerAssetLog.where(store_customer_asset_item_id: id)
+    store_customer_asset_logs.where(store_customer_asset_item_id: id)
   end
 end
