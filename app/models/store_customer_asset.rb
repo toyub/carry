4f062620f0c88adaa5cbdb53f191a5ffe4e72251
FaithTan <seventanyb@gmail.com>
2015-12-29 10:46:02 +0800

    组合使用判断

--- app/models/store_customer_asset.rb
+++ app/models/store_customer_asset.rb
@@ -2,4 +2,9 @@ class StoreCustomerAsset < ActiveRecord::Base
    has_many :items, class_name: 'StoreCustomerAssetItem'
 
    accepts_nested_attributes_for :items
+
+   def left?
+     items.each { |item| return true if item.left_quantity != 0 }
+     return false
+   end
 end
