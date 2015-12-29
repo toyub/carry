b3afa568fa090f30c0f5cb22fcfdd42b81096aa5
姜河 <jianghe@icar99.com>
2015-12-29 16:13:32 +0800

    核销

--- app/controllers/kucun/material_sales_controller.rb
+++ app/controllers/kucun/material_sales_controller.rb
@@ -1,7 +1,9 @@
 module Kucun
   class MaterialSalesController < BaseController
     def index
-      @store_material = StoreMaterial.find(params[:material_id])
+      @store_material = current_store.store_materials.find(params[:material_id])
+      @material_saleinfo = @store_material.store_material_saleinfo
+      @order_items = @material_saleinfo.store_order_items.includes(:store_order)
     end
   end
 end
