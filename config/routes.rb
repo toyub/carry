2d03afe3c931d2505634ce75fcd253d28422a8b5
朱士松 <zhushisong@icar99.com>
2015-12-29 09:33:31 +0800

    refact repayment

--- config/routes.rb
+++ config/routes.rb
@@ -340,7 +340,7 @@ Rails.application.routes.draw do
       resources :pre_orders, only: [:index]
       resources :complaints, only: [:index, :edit, :update]
       resources :store_trackings, only: [:index, :create]
-      resources :store_repayments do
+      resources :store_repayments, only: [:index, :create] do
         collection do
           get :finished, :all
         end
