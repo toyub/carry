ff5fc39e279896adf7c1c9f1e02b818037e71088
FaithTan <seventanyb@gmail.com>
2015-12-30 17:05:13 +0800

    add sale_category data into incomes#index

--- app/controllers/ais/incomes_controller.rb
+++ app/controllers/ais/incomes_controller.rb
@@ -1,6 +1,7 @@
 class Ais::IncomesController < Ais::BaseController
   def index
     @service_categories = StoreServiceCategory.all
+    @material_categories = SaleCategory.all
   end
 
   def search
--- app/controllers/ais/order_items_controller.rb
+++ app/controllers/ais/order_items_controller.rb
@@ -1,6 +1,9 @@
 class Ais::OrderItemsController < Ais::BaseController
   def index
-    @categories = StoreServiceCategory.find(params[:category_id])
-    @order_items = @categories.order_items
+    # 后台暂未修改StoreServiceCategory对应成ServiceCategory, 
+    # 且目前测试数据只有StoreServiceCategory才有，所以等修改
+    # 完成之后直接用Category来查找对应的销售类别
+    @category = StoreServiceCategory.find(params[:category_id])
+    @order_items = @category.order_items
   end
 end
