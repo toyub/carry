916f52228d1952d5c201a0a8d8a020c2e5684748
姜河 <jianghe@icar99.com>
2015-12-31 16:36:09 +0800

    订单打印

--- app/controllers/printer/pos/orders_controller.rb
+++ app/controllers/printer/pos/orders_controller.rb
@@ -2,6 +2,7 @@ module Printer
   module Pos
     class OrdersController < BaseController
         def show
+          @store_order = current_store.store_orders.find(params[:id])
         end
     end
   end
