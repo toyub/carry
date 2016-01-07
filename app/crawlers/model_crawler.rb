d86df25874707a03142eb3e113acb4f8319d05ea
radiocontroller <zsh302643159@163.com>
2016-01-07 16:40:49 +0800

    refact

--- app/crawlers/model_crawler.rb
+++ app/crawlers/model_crawler.rb
@@ -2,7 +2,6 @@ class ModelCrawler
   include Crawlable
 
   def process_page_results(page)
-    page.search('.interval01-list').shift
-    page.search('.interval01-list').search('li')
+    page.search('.interval01-list li')
   end
 end
