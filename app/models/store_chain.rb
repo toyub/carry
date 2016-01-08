01c28f7b832f2217b16a0eb9fa28b02928afed9f
FaithTan <seventanyb@gmail.com>
2016-01-08 17:20:30 +0800

    ci

--- app/models/store_chain.rb
+++ app/models/store_chain.rb
@@ -1,6 +1,6 @@
 class StoreChain < ActiveRecord::Base
   has_many :stores
-  belongs_to :head_office, class_name: 'Store', foreign_key: :admin_store_id
-
+  has_many :store_staff, through: :stores
   has_many :store_customers
+  belongs_to :head_office, class_name: 'Store', foreign_key: :admin_store_id
 end
