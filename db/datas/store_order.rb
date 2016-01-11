30be788067fe025468f6e748f9e3a11e947fbd46
issac <issaclau3@gmail.com>
2016-01-11 21:17:57 +0800

    change test data

--- db/datas/store_order.rb
+++ db/datas/store_order.rb
@@ -13,8 +13,8 @@ StoreOrder.create(
   store_customer: StoreCustomer.first,
   store_vehicle: StoreVehicle.first,
   items_attributes: [
-    { orderable: StoreMaterialSaleinfo.where(service_needed: true).first, price: 120, quantity: 20 },
-    { orderable: StoreMaterialSaleinfo.where(service_needed: true).first, price: 120, quantity: 20 },
+    { orderable: StoreMaterialSaleinfo.where(service_needed: true).first, price: 120, quantity: 2 },
+    { orderable: StoreMaterialSaleinfo.where(service_needed: true).first, price: 120, quantity: 2 },
     { orderable: StorePackage.first, price: 130, quantity: 2 },
     { orderable: StorePackage.first, price: 140, quantity: 2 },
     { orderable: StoreService.first, price: 260, quantity: 2 },
