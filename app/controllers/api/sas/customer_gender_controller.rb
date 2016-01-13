a8bf54ee6fa73e4385290b8700a0f6e2cdee2b27
tanyubin <tanyubin@icar99.com>
2016-01-13 16:10:38 +0800

    echart 显示数据顺序bug

--- app/controllers/api/sas/customer_gender_controller.rb
+++ app/controllers/api/sas/customer_gender_controller.rb
@@ -2,7 +2,7 @@ class Api::Sas::CustomerGenderController < Api::BaseController
   def index
     @data = {
       female: [StoreCustomer.female.store_group.count, StoreCustomer.female.membership!.count, StoreCustomer.female.membership.count ],
-      male: [StoreCustomer.male.store_group!.count, StoreCustomer.male.membership!.count, StoreCustomer.male.membership.count ],
+      male: [StoreCustomer.male.store_group.count, StoreCustomer.male.membership!.count, StoreCustomer.male.membership.count ],
     }
     render json: @data
   end
