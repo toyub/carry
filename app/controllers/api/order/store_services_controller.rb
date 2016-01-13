21eecded7e857e5dcf51a175d2db9aecc346c66d
朱士松 <zhushisong@icar99.com>
2016-01-13 16:43:20 +0800

    search service

--- app/controllers/api/order/store_services_controller.rb
+++ app/controllers/api/order/store_services_controller.rb
@@ -1,18 +1,18 @@
 module Api
   module Order
     class StoreServicesController < BaseController
-      before_action :get_service, only: [:service_categories]
+      def index
+        @services = @store.store_services.by_category(params[:service_category_id])
+        respond_with @services, location: nil
+      end
+
       def service_name
-        params[:q] = { name_or_service_category_name_cont: params[:name_cont] }
+        # params[:q] = { service_category_name_or_name_cont: params[:name_cont] }
         @q = @store.store_services.ransack(params[:q])
         @services = @q.result
         respond_with @services, location: nil
       end
 
-      private
-      def get_service
-        @service_category = ServiceCategory.where(id: params[:service_category_id]).last
-      end
     end
   end
 end
