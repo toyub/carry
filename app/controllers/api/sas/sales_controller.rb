fde4e5100fe65bb2c8244d7ca278b43d83489868
tanyubin <tanyubin@icar99.com>
2016-01-14 18:16:48 +0800

    统计数据所有的数据查找关联到所属门店下

--- app/controllers/api/sas/sales_controller.rb
+++ app/controllers/api/sas/sales_controller.rb
@@ -1,6 +1,8 @@
 class Api::Sas::SalesController < Api::BaseController
+  before_action :set_store
+
   def index
-    sales = StoreMonthConsumingSerializer.new.data
+    sales = StoreMonthConsumingSerializer.new(@store, Time.now.month).data
     @data = {
       months: sales.keys,
       figures: sales.values
@@ -9,21 +11,26 @@ class Api::Sas::SalesController < Api::BaseController
   end
 
   def payments
-    payments = StoreCustomerPayment.by_month
+    payments = @store.store_customer_payments.by_month
     @data = StoreMonthConsumingPaymentsSerializer.new(payments).data
     render json: @data
   end
 
   def categories
-    orderitems = StoreOrderItem.by_month
+    orderitems = @store.store_order_items.by_month
     @data = StoreMonthConsumingCategoriesSerializer.new(orderitems).data
     render json: @data
   end
 
   def days
-    orderitems = StoreOrderItem.by_month
+    orderitems = @store.store_order_items.by_month
     @data = StoreMonthConsumingDaysSerializer.new(orderitems).data
     render json: @data
+  end
+
+  private
+  def set_store
+    @store = Store.find_by_id(params[:id]) || current_store
   end
 
 end
--- app/serializers/store_month_consuming_serializer.rb
+++ app/serializers/store_month_consuming_serializer.rb
@@ -1,17 +1,17 @@
 class StoreMonthConsumingSerializer < ActiveModel::Serializer
   attr_accessor :data
 
-  def initialize(month = Time.now.month)
+  def initialize(store, month)
     @data = {}
     month = Time.now.month unless (1..12).include? month
-    set_data(month)
+    set_data(store, month)
   end
 
   private
-  def set_data(month = Time.now.month)
+  def set_data(store, month)
     ((month - 6)..month).each do |i|
       next if i < 1
-      @data[i.to_s + "月份"] = StoreMaterialSaleinfo.total_amount(i.month.ago)
+      @data[i.to_s + "月份"] = store.store_material_saleinfos.total_amount(i.month.ago)
     end
   end
 end
