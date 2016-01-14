14d6fc7a3fd00592e98d8f21b8b522befe7983dc
tanyubin <tanyubin@icar99.com>
2016-01-14 16:59:52 +0800

    ing

--- app/controllers/sas/customers_controller.rb
+++ app/controllers/sas/customers_controller.rb
@@ -1,4 +1,5 @@
 class Sas::CustomersController < Sas::BaseController
   def index
+    @store = Store.find_by_id(params[:store_id]) || current_store
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.integer  "orderable_id"
     t.integer  "quantity",                               null: false
     t.decimal  "price",          precision: 6, scale: 2, null: false
-    t.decimal  "amount",         precision: 8, scale: 2, null: false
+    t.decimal  "amount",         precision: 8, scale: 2, null: false, comment: "amount = price * quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "party_type"
@@ -135,7 +135,7 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.string   "party_type"
     t.integer  "party_id"
     t.string   "subject"
-    t.decimal  "amount",     precision: 10, scale: 2
+    t.decimal  "amount",     precision: 10, scale: 2,                 comment: "amount = sum(order_items.amount)"
     t.integer  "staffer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -165,6 +165,10 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.string   "itemable_type"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.decimal  "retail_price"
+    t.integer  "store_staff_id"
+    t.integer  "store_id"
+    t.integer  "store_chain_id"
   end
 
   create_table "recommended_orders", force: :cascade do |t|
@@ -1217,7 +1221,7 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.string   "content"
     t.integer  "delay_interval",   default: 0
     t.integer  "delay_unit"
-    t.integer  "trigger_timing",   default: 1
+    t.integer  "trigger_timing"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
   end
@@ -1394,9 +1398,9 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.boolean  "favorable",                                                 default: false
     t.integer  "setting_type",                                              default: 0
     t.integer  "store_service_id"
+    t.integer  "store_order_item_id"
     t.integer  "store_vehicle_id"
     t.integer  "store_order_id"
-    t.integer  "store_order_item_id"
     t.integer  "templateable_id"
     t.string   "templateable_type"
     t.integer  "category_id"
@@ -1455,6 +1459,7 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.integer  "store_workstation_id"
     t.string   "store_engineer_ids",              limit: 45
     t.integer  "store_service_setting_id"
+    t.integer  "store_order_item_id"
     t.boolean  "finished",                                    default: false
     t.integer  "used_time"
     t.json     "mechanics"
@@ -1464,7 +1469,6 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.integer  "elapsed"
     t.json     "overtimes",                                   default: []
     t.integer  "status",                                      default: 0
-    t.integer  "store_order_item_id"
   end
 
   create_table "store_service_workflows", force: :cascade do |t|
@@ -1553,20 +1557,20 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.string   "reason_for_leave"
     t.string   "numero"
     t.integer  "store_position_id"
+    t.json     "bonus",                                                       default: {}
+    t.decimal  "trial_salary",                       precision: 10, scale: 2
+    t.decimal  "regular_salary",                     precision: 10, scale: 2
+    t.decimal  "previous_salary",                    precision: 10, scale: 2
+    t.integer  "trial_period"
     t.integer  "store_employee_id"
+    t.json     "skills",                                                      default: {}
+    t.json     "other",                                                       default: {}
     t.string   "full_name"
     t.string   "phone_number"
     t.boolean  "mis_login_enabled",                                           default: false
     t.boolean  "app_login_enabled",                                           default: false
     t.boolean  "erp_login_enabled",                                           default: false
     t.integer  "roles",                                                                                             array: true
-    t.json     "bonus",                                                       default: {}
-    t.decimal  "trial_salary",                       precision: 10, scale: 2
-    t.decimal  "regular_salary",                     precision: 10, scale: 2
-    t.decimal  "previous_salary",                    precision: 10, scale: 2
-    t.integer  "trial_period"
-    t.json     "skills",                                                      default: {}
-    t.json     "other",                                                       default: {}
     t.boolean  "deduct_enabled",                                              default: false
     t.integer  "deadline_days"
     t.boolean  "contract_notice_enabled",                                     default: false
@@ -1582,6 +1586,10 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.string   "itemable_type"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "store_chain_id"
+    t.integer  "store_id"
+    t.integer  "store_staff_id"
+    t.decimal  "price"
   end
 
   create_table "store_subscribe_orders", force: :cascade do |t|
@@ -1737,7 +1745,6 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.integer  "workflow_id"
     t.string   "color"
     t.integer  "status",                                   default: 0
-    t.integer  "store_group_id"
   end
 
   create_table "stores", force: :cascade do |t|
@@ -1776,6 +1783,7 @@ ActiveRecord::Schema.define(version: 20160114053210) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "letter"
   end
 
   create_table "vehicle_engines", force: :cascade do |t|
