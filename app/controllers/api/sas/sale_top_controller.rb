242d6f95abda47f271751a62a842583b0c46069e
tanyubin <tanyubin@icar99.com>
2016-01-18 13:31:26 +0800

    调用修改之后的top排行方法

--- app/controllers/api/sas/sale_top_controller.rb
+++ app/controllers/api/sas/sale_top_controller.rb
@@ -1,10 +1,10 @@
 class Api::Sas::SaleTopController < Api::BaseController
 
   def index
-    @top_saler = StoreOrderItem.top_of_saler(params[:type])
-    @top_material = StoreOrderItem.by_month.materials.top(params[:type])
-    @top_service = StoreOrderItem.by_month.services.top(params[:type])
-    @top_package = StoreOrderItem.by_month.packages.top(params[:type])
+    @top_saler = StoreStaff.best_saler
+    @top_material = StoreMaterialSaleinfo.top_sales_by_month
+    @top_service = StoreService.top_sales_by_month
+    @top_package = StorePackage.top_sales_by_month
   end
 
 end
