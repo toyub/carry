3c1fa3050582831cf4f1f9fb5807e0afb4ab0c00
radiocontroller <zsh302643159@163.com>
2016-01-19 17:16:40 +0800

    commit

--- app/apis/v1/message_records.rb
+++ app/apis/v1/message_records.rb
@@ -4,6 +4,7 @@ module V1
     resource :message_records do
       add_desc '短信记录列表'
       get do
+        params[:q][:created_at_lteq] = Time.zone.parse(params[:q][:created_at_lteq]).end_of_day
         q = SmsRecord.ransack(params[:q])# 添加多态后需要条件查询
         present q.result(distince: true), with: ::Entities::MessageRecord
       end
