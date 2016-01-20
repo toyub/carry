df10317446aee6344e508753dd74c3cb597ffcdb
朱士松 <zhushisong@icar99.com>
2016-01-20 12:02:39 +0800

    rename

--- app/apis/entities/package_asset.rb
+++ app/apis/entities/package_asset.rb
@@ -9,10 +9,6 @@ module Entities
     expose(:name) {|model| model.workflowable_hash["name"]}
   end
 
-  class PackageAssetItem < Grape::Entity
-
-  end
-
   class PackageAsset < Grape::Entity
     expose :id, if: {type: :default}
     expose :package_name, if: {type: :default}
@@ -22,9 +18,9 @@ module Entities
     expose :contain_items, using: Contain, if: {type: :default}
     expose :items, using: Item, if: {type: :default}
 
-    expose(:created_at) {|model| model.created_at.strftime("%Y-%m-%d")}
-    expose(:numero) {|model| model.store_order.numero}
-    expose(:store_name) {|model| model.store.name}
+    expose(:created_at, if: {type: :full}) {|model| model.created_at.strftime("%Y-%m-%d")}
+    expose(:numero, if: {type: :full}) {|model| model.store_order.numero}
+    expose(:store_name, if: {type: :full}) {|model| model.store.name}
 
     private
     def contain_items
