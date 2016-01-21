e4e7467d14a6f811753b7273b39306b75e9c6ad8
radiocontroller <zsh302643159@163.com>
2016-01-21 16:23:56 +0800

    commit

--- app/apis/v1/customers.rb
+++ app/apis/v1/customers.rb
@@ -69,6 +69,17 @@ module V1
             material_asset = @customer.taozhuang_assets.find(params[:id])
             present material_asset, with: ::Entities::MaterialAssetInfo
           end
+
+          route_param :material_asset_id do
+            add_desc "资产-商品-项目消费明细"
+            params do
+              requires :id, type: Integer, desc: "项目ID"
+            end
+            get "material_items/:id", requirements: { id: /[0-9]*/ } do
+              material_asset = @customer.taozhuang_assets.find(params[:material_asset_id])
+              present material_asset.items.find(params[:id]), with: ::Entities::MaterialItem
+            end
+          end
         end
 
         resource :license_numbers do
@@ -85,85 +96,50 @@ module V1
           end
         end
 
+        add_desc "消费记录"
+        params do
+          optional :q, type: Hash, default: {} do
+            optional :plate_id_eq, type: Integer, desc: "车牌ID"
+            optional :created_at_gt, type: DateTime, desc: "开始时间"
+            optional :created_at_lt, type: DateTime, desc: "结束时间"
+          end
+        end
+        get "orders", requirements: { customer_id: /[0-9]*/ } do
+          q = @customer.orders.ransack(params[:q])
+          orders = q.result.order('id asc')
+          present orders, with: ::Entities::Order
+        end
+
+        resource :packaged_assets, desc: "套餐相关" do
+          add_desc "套餐列表"
+          get "/", requirements: { customer_id: /[0-9]*/ } do
+            present @customer.packaged_assets, with: ::Entities::PackageAsset, type: :list
+          end
+
+          add_desc "套餐查看"
+          params do
+            requires :id, type: Integer, desc: '套餐id'
+          end
+          get ":id", requirements: { id: /[0-9]*/ } do
+            package_asset = @customer.packaged_assets.find(params[:id])
+            present package_asset, with: ::Entities::PackageAsset, type: :default
+          end
+
+          route_param :package_asset_id do
+            add_desc "套餐组合-查看-项目消费明细"
+            params do
+              requires :id, type: Integer, desc: '项目id'
+            end
+            get "package_items/:id", requirements: { id: /[0-9]*/ } do
+              package_asset = @customer.packaged_assets.find(params[:package_asset_id])
+              package_asset_item = package_asset.items.find(params[:id])
+              present package_asset_item, with: ::Entities::PackageAsset, type: :full
+            end
+          end
+        end
+
       end
 
-
-      group do #group start
-        before do
-          @customer = current_store_chain.store_customers.find(params[:customer_id])
-        end
-        route_param :customer_id do
-          add_desc "资产-商品-项目消费明细"
-          params do
-            requires :material_asset_id, type: Integer, desc: "商品资产ID"
-            requires :customer_id, type: Integer, desc: "客户ID"
-            requires :id, type: Integer, desc: "项目ID"
-          end
-          get "material_assets/:material_asset_id/material_items/:id", requirements: { id: /[0-9]*/ } do
-            material_asset = @customer.taozhuang_assets.find(params[:material_asset_id])
-            present material_asset.items.find(params[:id]), with: ::Entities::MaterialItem
-          end
-
-          add_desc "消费记录"
-          params do
-            requires :customer_id, type: Integer, desc: '客户ID'
-            optional :q, type: Hash, default: {} do
-              optional :plate_id_eq, type: Integer, desc: "车牌ID"
-              optional :created_at_gt, type: DateTime, desc: "开始时间"
-              optional :created_at_lt, type: DateTime, desc: "结束时间"
-            end
-          end
-          get "orders", requirements: { customer_id: /[0-9]*/ } do
-            q = @customer.orders.ransack(params[:q])
-            orders = q.result.order('id asc')
-            present orders, with: ::Entities::Order
-          end
-
-          add_desc "车牌的显示"
-          params do
-            requires :customer_id, type: Integer, desc: "客户id"
-          end
-          get "license_numbers", requirements: { customer_id: /[0-9]*/ } do
-            present @customer.plates, with: ::Entities::LicenseNumber
-          end
-
-          resource :packaged_assets, desc: "套餐相关" do
-            add_desc "套餐列表"
-            params do
-              requires :customer_id, type: Integer, desc: '客户ID'
-            end
-            get "/", requirements: { customer_id: /[0-9]*/ } do
-              present @customer.packaged_assets, with: ::Entities::PackageAsset, type: :list
-            end
-
-            add_desc "套餐查看"
-            params do
-              requires :customer_id, type: Integer, desc: '客户ID'
-              requires :id, type: Integer, desc: '套餐id'
-            end
-            get ":id", requirements: { id: /[0-9]*/ } do
-              package_asset = @customer.packaged_assets.find(params[:id])
-              present package_asset, with: ::Entities::PackageAsset, type: :default
-            end
-            
-            route_param :package_asset_id do
-              add_desc "套餐组合-查看-项目消费明细"
-              params do
-                requires :customer_id, type: Integer, desc: '客户ID'
-                requires :package_asset_id, type: Integer, desc: "套餐资产id"
-                requires :id, type: Integer, desc: '项目id'
-              end
-              get "package_items/:id", requirements: { id: /[0-9]*/ } do
-                package_asset = @customer.packaged_assets.find(params[:package_asset_id])
-                package_asset_item = package_asset.items.find(params[:id])
-                present package_asset_item, with: ::Entities::PackageAsset, type: :full
-              end
-            end
-          end
-
-        end
-     end
-     #group end
     end
 
   end
