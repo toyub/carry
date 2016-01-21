3659e35a79f10b733a9dc8f09e7d3f4a71a9b091
radiocontroller <zsh302643159@163.com>
2016-01-21 17:45:53 +0800

    delete space

--- app/apis/entities/material.rb
+++ app/apis/entities/material.rb
@@ -1,17 +1,17 @@
 module Entities
   class Material < Grape::Entity
     expose :id
-    expose (:name) { |material, options| material.store_material.name }
-    expose (:store_name) { |material, options| material.store.name }
+    expose(:name) { |material, options| material.store_material.name }
+    expose(:store_name) { |material, options| material.store.name }
     expose :barcode
     expose :speci
-    expose (:unit) { |material, options|  material.store_material.store_material_unit.try :name}
+    expose(:unit) { |material, options|  material.store_material.store_material_unit.try :name}
     expose :retail_price
     expose :bargain_price
     expose :point
-    expose (:inventory) { |material, options| material.store_material.inventory }
+    expose(:inventory) { |material, options| material.store_material.inventory }
     expose :sold_count
-    expose (:category) { |material, options| material.category.try :name }
+    expose(:category) { |material, options| material.category.try :name }
 
     private
 
