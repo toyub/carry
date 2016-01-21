2f22e588c7465e7e9e9a7aabbf951f0faca77dd2
朱士松 <zhushisong@icar99.com>
2016-01-21 10:26:48 +0800

    fix nil name

--- app/apis/entities/order.rb
+++ app/apis/entities/order.rb
@@ -1,7 +1,7 @@
 module Entities
   class OrderItem < Grape::Entity
     expose(:mechanics) {|model, options| model.workflow_mechanics.map(&->(wm){wm.engineer})}
-    expose(:service_name)  {|model, options| model.store_service_snapshot.name}
+    expose(:service_name)  {|model, options| model.store_service_snapshot.try(:name)}
     expose :price, :quantity, :discount, :amount
   end
 
