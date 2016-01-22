72ac405e206dd83741e43342c2b0c1c838223acd
朱士松 <zhushisong@icar99.com>
2016-01-22 17:46:54 +0800

    materials

--- app/apis/entities/material.rb
+++ app/apis/entities/material.rb
@@ -3,29 +3,20 @@ module Entities
     expose :id
     expose(:name) { |material, options| material.name }
     expose(:store_name) {|model| model.store.name}
-    expose(:barcode, if: :full) {|model| saleinfo(model, barcode)}
-    expose(:speci, if: :full) {|model| saleinfo(model,speci)}
+    expose(:barcode) {|model| model.store_material_saleinfo.try(:barcode) }
+    expose(:speci) {|model| model.store_material_saleinfo.try(:speci)}
     expose(:unit) { |material, options|  material.store_material_unit.try :name}
-    expose(:retail_price, if: :full) {|model| saleinfo(model,retail_price)}
-    expose(:bargain_price, if: :full) {|model| saleinfo(model, bargain_price)}
-    expose(:point, if: :full) {|model| saleinfo(model, point)}
+    expose(:retail_price) {|model| model.store_material_saleinfo.try(:retail_price)}
+    expose(:bargain_price) {|model| model.store_material_saleinfo.try(:bargain_price)}
+    expose(:point) {|model| model.store_material_saleinfo.try(:point)}
     expose :inventory
     expose :sold_count
-    # expose(:category) { |material, options| material.category.try :name }
     expose(:category, if: :full) {|model| model.store_material_saleinfo.category.try :name}
 
     private
 
       def sold_count
         500
-      end
-
-      def full
-        object.store_material_saleinfo.present?
-      end
-
-      def saleinfo(model,info)
-        model.store_material_saleinfo.info
       end
   end
 end
