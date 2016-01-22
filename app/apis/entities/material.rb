1dcc57f0efc5575255b83c39b914edc481f545fd
朱士松 <zhushisong@icar99.com>
2016-01-22 17:33:35 +0800

    fix materials

--- app/apis/entities/material.rb
+++ app/apis/entities/material.rb
@@ -3,20 +3,29 @@ module Entities
     expose :id
     expose(:name) { |material, options| material.name }
     expose(:store_name) {|model| model.store.name}
-    # expose :barcode
-    # expose :speci
-    # expose(:unit) { |material, options|  material.store_material.store_material_unit.try :name}
-    # expose :retail_price
-    # expose :bargain_price
-    # expose :point
-    # expose(:inventory) { |material, options| material.store_material.inventory }
-    # expose :sold_count
+    expose(:barcode, if: :full) {|model| saleinfo(model, barcode)}
+    expose(:speci, if: :full) {|model| saleinfo(model,speci)}
+    expose(:unit) { |material, options|  material.store_material_unit.try :name}
+    expose(:retail_price, if: :full) {|model| saleinfo(model,retail_price)}
+    expose(:bargain_price, if: :full) {|model| saleinfo(model, bargain_price)}
+    expose(:point, if: :full) {|model| saleinfo(model, point)}
+    expose :inventory
+    expose :sold_count
     # expose(:category) { |material, options| material.category.try :name }
+    expose(:category, if: :full) {|model| model.store_material_saleinfo.category.try :name}
 
     private
 
       def sold_count
         500
+      end
+
+      def full
+        object.store_material_saleinfo.present?
+      end
+
+      def saleinfo(model,info)
+        model.store_material_saleinfo.info
       end
   end
 end
