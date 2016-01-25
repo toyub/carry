190232d2fe815f3ca573e9f294c7c651bcdc7cd1
朱士松 <zhushisong@icar99.com>
2016-01-25 10:43:58 +0800

    search

--- app/apis/v1/materials.rb
+++ app/apis/v1/materials.rb
@@ -10,10 +10,10 @@ module V1
       params do
         optional :q, type: Hash, default: {} do
           optional :store_id_eq, type: Integer, desc: "所属门店ID"
-          optional :store_material_name_cont, type: String, desc: "商品名称"
-          optional :sale_category_id_eq, type: Integer, desc: "销售类别"
-          optional :retail_price_gteq, type: Float, desc: "价格区间-最小价格"
-          optional :retail_price_lteq, type: Float, desc: "价格区间-最大价格"
+          optional :name_cont, type: String, desc: "商品名称"
+          optional :store_material_saleinfo_sale_category_id_eq, type: Integer, desc: "销售类别"
+          optional :store_material_saleinfo_retail_price_gteq, type: Float, desc: "价格区间-最小价格"
+          optional :store_material_saleinfo_retail_price_lteq, type: Float, desc: "价格区间-最大价格"
           optional :created_at_gteq, type: String, desc: "上架时间"
           optional :s, type: String, desc: "价格排序"
         end
