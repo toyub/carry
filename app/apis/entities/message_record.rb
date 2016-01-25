4248bfa5d03f38ed38b58a7ee16fee38d9c7a71e
tanyubin <tanyubin@icar99.com>
2016-01-25 13:02:27 +0800

    修改短信记录查询返回category的内容

--- app/apis/entities/message_record.rb
+++ app/apis/entities/message_record.rb
@@ -3,8 +3,14 @@ module Entities
     expose :content
     expose :customer_name
     expose :phone_number
-    expose(:category) { |record, options| record.first_category }
+    expose :category
     expose(:store_name) { |record, options| record.store.try(:name) }
     expose(:created_at) { |record, options| record.created_at.strftime('%Y-%m-%d') }
+
+    private
+    def category
+      first_category = SmsSwitchType.find(object.first_category.to_i)
+      "#{first_category.name[:type]}-#{first_category.name[:sub_category].constantize.find(object.second_category.to_i).name}"
+    end
   end
 end
