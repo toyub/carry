ebd5433143d15c2fb9dd681b9081cdd8f392524b
朱士松 <zhushisong@icar99.com>
2016-01-28 16:57:47 +0800

    add vehicle

--- app/services/add_vehicle_for_ipad_service.rb
+++ app/services/add_vehicle_for_ipad_service.rb
@@ -8,7 +8,7 @@ class AddVehicleForIpadService
     @customer_params = options[:customer_params]
     @vehicle_params = vehicle_params
     @plate_params = plate_params
-    @customer = options[:object]
+    @customer = options[:customer]
   end
 
   def call
