8c40824ec71e1a6052bced63514addd2874b156d
姜河 <jianghe@icar99.com>
2016-02-01 10:09:41 +0800

    POS 订单-删除订单项

--- app/controllers/api/pos/carts/items_controller.rb
+++ app/controllers/api/pos/carts/items_controller.rb
@@ -2,9 +2,15 @@ module Api
   module Pos
     module Carts
       class ItemsController < Api::BaseController
-
         def show
           render json: {}
+        end
+
+        def destroy
+          order = current_store.store_orders.find(params[:order_id])
+          item = order.items.find(params[:id])
+          item.destroy!
+          respond_with item,location: nil
         end
       end
     end
