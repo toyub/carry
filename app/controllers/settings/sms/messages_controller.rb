7675f66e583e7ca69b46d30178728e8b8039d6fa
Wang Yingying <wangyingying@icar99.com>
2016-02-02 17:29:45 +0800

    message list

--- app/controllers/settings/sms/messages_controller.rb
+++ app/controllers/settings/sms/messages_controller.rb
@@ -1,8 +1,11 @@
 module Settings
   class Sms::MessagesController < BaseController
     def index
-      @topups = 5.times
-      @sms_balance = current_store.sms_balance
+      if params[:start_date].present?
+        @records = SmsRecord.where(created_at: params[:start_date] .. params[:end_date])
+      else
+        @records = SmsRecord.all
+      end
     end
   end
 end
