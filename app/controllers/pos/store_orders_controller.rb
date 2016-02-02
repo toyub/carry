31a9d0b746cc79b3b207d6045f6a8fbfcf0657d6
姜河 <jianghe@icar99.com>
2016-02-02 15:35:29 +0800

    POS 开单-查询 添加查询条件

--- app/controllers/pos/store_orders_controller.rb
+++ app/controllers/pos/store_orders_controller.rb
@@ -8,10 +8,14 @@ module Pos
     def new
       @super_material_categories = current_store.store_material_categories.super_categories
       @store_material_brands = current_store.store_material_brands
+      @service_categories = ServiceCategory.all
       @order = current_store.store_orders.new
     end
 
     def edit
+      @super_material_categories = current_store.store_material_categories.super_categories
+      @store_material_brands = current_store.store_material_brands
+      @service_categories = ServiceCategory.all
       @order = current_store.store_orders.find(params[:id])
     end
 
