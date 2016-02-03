980f3d7194a0bc579664d10abbf9d8a9127b4853
朱士松 <zhushisong@icar99.com>
2016-02-03 11:04:01 +0800

    fix api

--- app/apis/entities/vehicle_plate.rb
+++ app/apis/entities/vehicle_plate.rb
@@ -3,11 +3,11 @@ module Entities
     expose :store_customer_id, :license_number
     expose(:store_name) {|model| model.store.name}
     expose(:plate_id) {|model| model.id}
-    expose(:customer_name) {|model| model.store_customer.full_name}
+    expose(:customer_name) {|model| model.store_customer.try(:name)}
     expose(:phone_number) {|model| model.store_customer.try(:phone_number)}
     expose(:bought_on) {|model| model.store_vehicles.last.try(:detail).try(:bought_on,[])}
     expose(:barnd_name) {|model| model.store_vehicles.last.try(:vehicle_brand).try(:name)}
     expose(:series_name) {|model| model.store_vehicles.last.try(:vehicle_serie).try(:name)}
-    expose(:vip) {|model| model.store_customer_entity.try(:membership) == true ? true : false}
+    expose(:vip) {|model| model.store_customer.try(:store_customer_entity).try(:membership) == true ? true : false}
   end
 end
