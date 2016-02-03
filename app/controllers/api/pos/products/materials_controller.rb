273476c3b7a9649c7a7b0e19526212272fc0707b
姜河 <jianghe@icar99.com>
2016-02-03 11:20:17 +0800

    POS 开单-搜索

--- app/controllers/api/pos/products/materials_controller.rb
+++ app/controllers/api/pos/products/materials_controller.rb
@@ -3,10 +3,10 @@ module Api
     module Products
       class MaterialsController < Api::BaseController
         def index
-          @materials = current_store.store_material_saleinfos.joins(:store_material)
-          @materials = @materials.where('store_materials.store_material_root_category_id' => params[:store_material_root_category_id]) if params[:store_material_root_category_id]
-          @materials = @materials.where('store_materials.store_material_category_id' => params[:store_material_category_id]) if params[:store_material_category_id]
-          @materials = @materials.where('store_materials.store_material_brand_id' => params[:store_material_brand_id]) if params[:store_material_brand_id]
+          @materials = current_store.store_material_saleinfos.joins(:store_material).where(store_materials: {permitted_to_saleable: true})
+          @materials = @materials.where('store_materials.store_material_root_category_id' => params[:store_material_root_category_id]) if params[:store_material_root_category_id].present?
+          @materials = @materials.where('store_materials.store_material_category_id' => params[:store_material_category_id]) if params[:store_material_category_id].present?
+          @materials = @materials.where('store_materials.store_material_brand_id' => params[:store_material_brand_id]) if params[:store_material_brand_id].present?
           @materials = @materials.where('store_materials.name like :keyword', keyword: "%#{params[:keyword]}%") if params[:keyword].present?
           @materials = @materials.page(params[:page])
         end
