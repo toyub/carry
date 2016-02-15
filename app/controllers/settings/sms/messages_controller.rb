11a769763c1df2243586a0c56111fbc03720e3f6
Wang Yingying <wangyingying@icar99.com>
2016-02-15 16:05:05 +0800

    修改

--- app/controllers/settings/sms/messages_controller.rb
+++ app/controllers/settings/sms/messages_controller.rb
@@ -4,10 +4,11 @@ module Settings
     def index
       @records = current_store.sms_records
       if params[:start_date].present?
-        @records = SmsRecord.by_date(params[:start_date], params[:end_date])
-      elsif params[:category].present?
+        @records = @records.by_date(params[:start_date], params[:end_date])
+      end
+      if params[:category].present?
         first_category, second_category = params[:category].split("-")
-        @records = @records.by_category(  first_category, second_category )
+        @records = @records.by_category(first_category, second_category)
       end
 
     end
