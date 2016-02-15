de2893c956549e4817636bf6b2ca469fc54fc77f
Wang Yingying <wangyingying@icar99.com>
2016-02-15 10:10:11 +0800

    select option

--- app/controllers/settings/sms/messages_controller.rb
+++ app/controllers/settings/sms/messages_controller.rb
@@ -1,11 +1,14 @@
 module Settings
   class Sms::MessagesController < BaseController
     def index
+      @records = SmsRecord.all
       if params[:start_date].present?
         @records = SmsRecord.where(created_at: params[:start_date] .. params[:end_date])
-      else
-        @records = SmsRecord.all
+      elsif params[:category].present?
+        first_category, second_category = params[:category].split("-")
+        @records = @records.where(first_category: first_category).where(second_category: second_category)
       end
+
     end
   end
 end
