b018db4b26a365f9cb4bbb0e29b23bdcf17a4908
Wang Yingying <wangyingying@icar99.com>
2016-02-16 12:52:42 +0800

    修改业务员开单->

--- db/schema.rb
+++ db/schema.rb
@@ -1411,9 +1411,9 @@ ActiveRecord::Schema.define(version: 20160128054228) do
     t.boolean  "favorable",                                                 default: false
     t.integer  "setting_type",                                              default: 0
     t.integer  "store_service_id"
-    t.integer  "store_order_item_id"
     t.integer  "store_vehicle_id"
     t.integer  "store_order_id"
+    t.integer  "store_order_item_id"
     t.integer  "templateable_id"
     t.string   "templateable_type"
     t.integer  "category_id"
@@ -1468,7 +1468,6 @@ ActiveRecord::Schema.define(version: 20160128054228) do
     t.integer  "store_workstation_id"
     t.string   "store_engineer_ids",              limit: 45
     t.integer  "store_service_setting_id"
-    t.integer  "store_order_item_id"
     t.boolean  "finished",                                    default: false
     t.integer  "used_time"
     t.json     "mechanics"
@@ -1478,6 +1477,7 @@ ActiveRecord::Schema.define(version: 20160128054228) do
     t.integer  "elapsed"
     t.json     "overtimes",                                   default: []
     t.integer  "status",                                      default: 0
+    t.integer  "store_order_item_id"
     t.integer  "mechanic_commission_template_id"
   end
 
