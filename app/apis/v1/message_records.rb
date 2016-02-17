6b6fe9941e0faaea37e59bd7a7be8ee5a3adf2c6
tanyubin <tanyubin@icar99.com>
2016-02-17 16:14:09 +0800

    修改短信记录查询结果

--- app/apis/v1/message_records.rb
+++ app/apis/v1/message_records.rb
@@ -20,7 +20,7 @@ module V1
       get do
         created_at_lteq = params[:q][:created_at_lteq]
         created_at_lteq = Time.zone.parse(created_at_lteq).end_of_day if created_at_lteq.present?
-        params[:q][:first_category_eq], params[:q][:second_category_eq] = params[:q][:category_eq].scan(/\d{1,2}/) if params[:q][:category_eq].present?
+        params[:q][:first_category_eq], params[:q][:second_category_eq] = params[:q][:category_eq].split('-') if params[:q][:category_eq].present?
         q = SmsRecord.by_store.ransack(params[:q])
         present q.result(distince: true).order('created_at desc'), with: ::Entities::MessageRecord
       end
