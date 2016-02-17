00c9fded03b7638045dd027ed580a9d02a2be559
tanyubin <tanyubin@icar99.com>
2016-02-17 13:50:38 +0800

    amend tracking worker

--- app/jobs/order_tracking_job.rb
+++ app/jobs/order_tracking_job.rb
@@ -3,15 +3,14 @@ class OrderTrackingJob < ActiveJob::Base
 
   def perform(order)
     order.items.each do |item|
-      switch_id = SmsSwitchType.find_by_name(item.orderable_type).id
-      next unless StoreSwitch.by_type(item.orderable_type, switch_id).first.enabled
+      next unless StoreSwitch.by_switchable_type("SmsTrackingSwitchType").enabled? item.orderable_type
       item.orderable.trackings.each do |tracking|
         options = {
           store_id:         order.store_id,
           customer_id:      order.store_customer_id,
           content:          tracking.content,
           first_category:   "SmsTrackingSwitchType",
-          second_category:  switch_id
+          second_category:  StoreSwitch::SaleCategory[item.orderable_type.to_sym]
         }
         SmsJob.set(wait: tracking.delay_until).perform_later(options)
       end
--- app/models/store_switch.rb
+++ app/models/store_switch.rb
@@ -3,7 +3,18 @@ class StoreSwitch < ActiveRecord::Base
   validates :switchable_id, presence: true
   validates :switchable_type, presence: true
 
-  scope :by_type, ->(type, id) { where(switchable_type: type, switchable_id: id) }
+  scope :by_switchable_type, ->(type) { where(switchable_type: type) }
+  scope :by_switchable_id, ->(id) { where(switchable_id: id) }
+
+  SaleCategory = {
+    "StoreService": 0,
+    "StoreMaterialSaleinfo": 1,
+    "StorePackage": 2,
+  }
+
+  def self.enabled?(type)
+    by_switchable_id(SaleCategory[type.to_sym]).first.enabled
+  end
 
   def switchable
     self.switchable_type.constantize.find(self.switchable_id)
