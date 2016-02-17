6f21548a66e35b979411c21c6befba281692f11a
tanyubin <tanyubin@icar99.com>
2016-02-17 14:58:18 +0800

    考虑施工的回访

--- app/jobs/order_tracking_job.rb
+++ app/jobs/order_tracking_job.rb
@@ -4,6 +4,7 @@ class OrderTrackingJob < ActiveJob::Base
   def perform(order)
     order.items.each do |item|
       next unless StoreSwitch.by_switchable_type("SmsTrackingSwitchType").enabled? item.orderable_type
+      service_tracking(order, item.orderable.services) if item.orderable == "StoreMaterialSaleinfo" && item.orderable.service_needed && item.orderable.services.present?
       item.orderable.trackings.each do |tracking|
         options = {
           store_id:         order.store_id,
@@ -16,5 +17,20 @@ class OrderTrackingJob < ActiveJob::Base
         CustomerTrackingJob.set(wait: tracking.delay_until).perform_later(options)
       end
     end
+  end
+
+  private
+  def service_tracking(order, trackings)
+    trackings.each do |tracking|
+      next unless tracking.tracking_needed
+      options = {
+        store_id:         order.store_id,
+        customer_id:      order.store_customer_id,
+        content:          tracking.tracking_content,
+        first_category:   "SmsTrackingSwitchType",
+        second_category:  1
+      }
+      SmsJob.set(wait: tracking.delay_until).perform_later(options)
+      CustomerTrackingJob.set(wait: tracking.delay_until).perform_later(options)
   end
 end
