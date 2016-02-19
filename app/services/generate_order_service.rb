8f0e26ef1b7d65f54fd033e94bcb92dfd1afaed3
朱士松 <zhushisong@icar99.com>
2016-02-19 17:19:48 +0800

    generate service to order items

--- app/services/generate_order_service.rb
+++ app/services/generate_order_service.rb
@@ -9,32 +9,38 @@ class GenerateOrderService
   end
 
   def call
-    binding.pry
+    # binding.pry
     ActiveRecord::Base.transaction do
-      # @order_params[:materials] = [{"material_id"=>6, "count"=>200}, {"material_id"=>1, "count"=>300}]
-      material_ids = @order_params[:materials].map{|m| m[:material_id]}
-      StoreMaterial.where(id: material_ids).each_with_index do |material, i|
-        StoreMaterialSaleinfo.create!(@basic_params.merge(store_material_id: material.id)) unless material.store_material_saleinfo.present?
-        material_item = material.store_material_saleinfo.store_order_items.new(material_order_params(i))
-        @order_items << material_item
-      end
-      @customer.orders.create!(generate_order_params.merge(items: @order_items))
+      generate_material_to_items
+      generate_service_to_item
+      @customer.orders.create!(order_params_merge_vehicle.merge(items: @order_items))
     end
     true
   rescue ActiveRecord::RecordInvalid
     false
   end
 
-  def generate_order_params
-    @basic_params.merge(
-      store_vehicle_id: @order_params[:vehicle_id]
-    )
+  def generate_material_to_items
+    material_ids = @order_params[:materials].map{|m| m[:material_id]}
+    StoreMaterial.where(id: material_ids).each_with_index do |material, i|
+      StoreMaterialSaleinfo.create!(@basic_params.merge(store_material_id: material.id)) unless material.store_material_saleinfo.present?
+      material_item = material.store_material_saleinfo.store_order_items.new(material_item_params(i))
+      @order_items << material_item
+    end
   end
 
-  def material_order_params(i)
+  def generate_service_to_item
+    service_ids = @order_params[:services].map{|s| s[:service_id]}
+    StoreService.where(id: service_ids).each_with_index do |service, i|
+      service_item = service.store_order_items.new(service_ietm_params(i))
+      @order_items << service_item
+    end
+  end
+
+  def material_item_params(i)
     @basic_params.merge(
       quantity: @order_params[:materials][i][:count],
-      price: final_price(i),
+      price: material_final_price(i),
       store_customer_id: @order_params[:materials][i][:store_customer_id],
       discount: @order_params[:materials][i][:discount],
       discount_reason: @order_params[:materials][i][:discount_reason],
@@ -42,11 +48,41 @@ class GenerateOrderService
     )
   end
 
-  def final_price(i)
-    if @order_params[:is_vip] && @order_params[:materials][i][:vip_price].present?
-      pay = @order_params[:materials][i][:vip_price] - @order_params[:materials][i][:discount].to_f
+  def material_final_price(i)
+    if @order_params[:materials][i][:from_asset]
+      0
+    elsif @order_params[:is_vip] && @order_params[:materials][i][:vip_price].present?
+      @order_params[:materials][i][:vip_price] - @order_params[:materials][i][:discount].to_f
     else
-      pay = @order_params[:materials][i][:price] - @order_params[:materials][i][:discount].to_f
+      @order_params[:materials][i][:price] - @order_params[:materials][i][:discount].to_f
     end
+  end
+
+  def service_ietm_params(i)
+    @basic_params.merge(
+      quantity: @order_params[:services][i][:count],
+      price: service_final_price(i),
+      store_customer_id: @order_params[:services][i][:store_customer_id],
+      discount: @order_params[:services][i][:discount],
+      discount_reason: @order_params[:services][i][:discount_reason],
+      vip_price: @order_params[:services][i][:vip_price],
+      amount: service_final_price(i) * @order_params[:services][i][:count]
+    )
+  end
+
+  def service_final_price(i)
+    if @order_params[:services][i][:from_asset]
+      0
+    elsif @order_params[:is_vip] && @order_params[:services][i][:vip_price].present?
+      @order_params[:services][i][:vip_price] - @order_params[:services][i][:discount].to_f
+    else
+      @order_params[:services][i][:price] - @order_params[:services][i][:discount].to_f
+    end
+  end
+
+  def order_params_merge_vehicle
+    @basic_params.merge(
+      store_vehicle_id: @order_params[:vehicle_id]
+    )
   end
 end
