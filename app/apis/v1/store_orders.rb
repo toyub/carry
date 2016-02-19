113a57a8024d4ae58f29396aa8caafe198fffaf2
朱士松 <zhushisong@icar99.com>
2016-02-19 11:30:17 +0800

    fix

--- app/apis/v1/store_orders.rb
+++ app/apis/v1/store_orders.rb
@@ -11,17 +11,17 @@ module V1
         requires :is_vip, type: Boolean, desc: '是否是会员客户'
         requires :platform, type: String, desc: '验证平台！'
         requires :customer_id, type: Integer, desc: '客户id'
-        optional :material, type: Array do
-          optional :count, type: Integer, desc: '商品的数量'
-          optional :material_id, type: Integer, desc: '商品的id'
-          optional :bargain_price, type: BigDecimal, desc: '优惠价'
+        optional :material, type: Hash, default: {} do
+          optional :material_id, type: String, desc: '商品的id'
+          optional :count, type: String, desc: '商品的数量'
+          optional :bargain_price, type: String, desc: '优惠价'
         end
-        optional :service, type: Array, default: {} do
-          optional :count, type: Integer, desc: '服务的数量'
-          optional :service_id, type: Integer, desc: '服务的id'
-          optional :bargain_price, type: BigDecimal, desc: '优惠价'
+        optional :service, type: Hash, default: {} do
+          optional :service_id, type: String, desc: '服务的id'
+          optional :count, type: String, desc: '服务的数量'
+          optional :bargain_price, type: String, desc: '优惠价'
         end
-        optional :Package, type: Array, default: {} do
+        optional :package, type: Hash, default: {} do
           optional :package_id, type: Integer, desc: '套餐的id'
           optional :count, type: Integer, desc: '套餐的数量'
           optional :bargain_price, type: BigDecimal, desc: '优惠价'
@@ -30,6 +30,14 @@ module V1
 
       get do
         binding.pry
+
+        material_ids = order_params[:material][:material_id].split(",")
+        if params[:is_vip]
+          StoreMaterial.where(id: material_ids).each do |material|
+            
+          end
+        else
+        end
         present info: 444
       end
     end
@@ -39,8 +47,38 @@ module V1
         @order = ActionController::Parameters.new(params)
       end
 
-      def material_params
-        @order.permit()
+      def basic_params
+        params[:store_id] = current_store.id
+        params[:store_chain_id] = current_store_chain.id
+        params[:store_staff_id] = current_user.id
+      end
+
+      def order_params
+        params_permit
+        basic_params
+        @order.permit(
+          :is_vip,
+          :platform,
+          :customer_id,
+          :store_id,
+          :store_chain_id,
+          :store_staff_id,
+          material: [
+            :material_id,
+            :bargain_price,
+            :count
+          ],
+          service: [
+            :service_id,
+            :bargain_price,
+            :count
+          ],
+          package: [
+            :package_id,
+            :bargain_price,
+            :count
+          ]
+        )
       end
     end
 
