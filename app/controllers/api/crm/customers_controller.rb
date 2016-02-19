831e6799e50ba83bca47f30533b4f4279037f96d
tanyubin <tanyubin@icar99.com>
2016-02-19 10:49:04 +0800

    need to create customer entity as well

--- app/controllers/api/crm/customers_controller.rb
+++ app/controllers/api/crm/customers_controller.rb
@@ -16,23 +16,22 @@ module Api
           c.store_chain_id =  current_store.store_chain.id
           c.store_staff_id =  current_user.id
         end
+        basic_params = {
+          store_id: current_store.id,
+          store_chain_id: current_store.store_chain.id,
+          store_staff_id: current_user.id,
+        }
+        customer.create_store_customer_entity(basic_params)
         vehicle = customer.store_vehicles.create!(
-          {
-            store_id: current_store.id,
-            store_chain_id: current_store.store_chain.id,
-            store_staff_id: current_user.id,
-            detail: {bought_on: params[:vehicle][:detail][:bought_on]}
-          }
+          basic_params.merge({detail: {bought_on: params[:vehicle][:detail][:bought_on]}})
         )
         plate = customer.plates.create!(
-          {
-            store_id: current_store.id,
-            store_chain_id: current_store.store_chain.id,
-            store_staff_id: current_user.id,
+          basic_params.merge({
             store_vehicle_id: vehicle.id,
             license_number: params[:vehicle][:license_number]
-          }
+          })
         )
+        vehicle.vehicle_plates.create!(store_vehicle_registration_plate_id: plate.id)
         Status.new(success: true, notice: "添加#{plate.license_number}成功!")
       end
     end
