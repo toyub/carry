0d4726d615693d78dacc8d45078c0f4d3e195cc8
tanyubin <tanyubin@icar99.com>
2016-02-19 16:59:02 +0800

    amend bug of this branch

--- app/controllers/api/crm/customers_controller.rb
+++ app/controllers/api/crm/customers_controller.rb
@@ -9,30 +9,35 @@ module Api
 
       private
       def check_vehicle_info
-        return Status.new(success: true, notice: '车辆已存在!') if StoreVehicleRegistrationPlate.find_by(license_number: params[:vehicle][:license_number])
-        customer = current_store.store_customers.find_or_create_by(phone_number: params[:vehicle][:phone_number]) do |c|
-          c.first_name = params[:vehicle][:customer][:first_name]
-          c.last_name = params[:vehicle][:customer][:last_name]
-          c.store_chain_id =  current_store.store_chain.id
-          c.store_staff_id =  current_user.id
+        return Status.new(success: true, notice: '车辆已存在!') if current_store.store_vehicle_registration_plates.find_by(license_number: params[:vehicle][:license_number])
+        customer = current_store.store_customers.find_by(phone_number: params[:vehicle][:phone_number])
+        unless customer.present?
+          customer = current_store.store_customers.create!(customer_params)
+          entity = customer.create_store_customer_entity(customer_relative_params)
+          entity.create_store_customer_settlement(customer_relative_params)
         end
-        basic_params = {
-          store_id: current_store.id,
-          store_chain_id: current_store.store_chain.id,
-          store_staff_id: current_user.id,
-        }
-        customer.create_store_customer_entity(basic_params)
-        vehicle = customer.store_vehicles.create!(
-          basic_params.merge({detail: {bought_on: params[:vehicle][:detail][:bought_on]}})
-        )
-        plate = customer.plates.create!(
-          basic_params.merge({
-            store_vehicle_id: vehicle.id,
-            license_number: params[:vehicle][:license_number]
-          })
-        )
-        vehicle.vehicle_plates.create!(store_vehicle_registration_plate_id: plate.id)
+        vehicle = customer.store_vehicles.create!(vehicle_params)
+        plate = vehicle.plates.create!(plate_params)
         Status.new(success: true, notice: "添加#{plate.license_number}成功!")
+      end
+
+      def customer_params
+        customer = append_store_attrs ActionController::Parameters.new(first_name: params[:vehicle][:customer][:first_name], last_name: params[:vehicle][:customer][:last_name], phone_number: params[:vehicle][:phone_number])
+        customer.permit(:store_id, :store_staff_id, :first_name, :last_name, :phone_number)
+      end
+
+      def customer_relative_params
+        (append_store_attrs ActionController::Parameters.new).permit(:store_id, :store_staff_id).to_h
+      end
+
+      def vehicle_params
+        vehicle = append_store_attrs ActionController::Parameters.new(detail: params[:vehicle][:detail])
+        vehicle.permit(:store_id, :store_staff_id, detail: [:bought_on])
+      end
+
+      def plate_params
+        plate = append_store_attrs ActionController::Parameters.new(license_number: params[:vehicle][:license_number])
+        plate.permit(:store_id, :store_staff_id, :license_number)
       end
     end
   end
