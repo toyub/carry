bc9026a2d53ea9d26686ec8030a46258e96b3c36
tanyubin <tanyubin@icar99.com>
2016-02-22 14:46:10 +0800

    修改短信发送worker及密码找回验证码发送api

---
+++ app/apis/v1/send_captcha_code.rb
@@ -0,0 +1,34 @@
+module V1
+  class SendCaptchaCode < Grape::API
+
+    resource :send_captcha_code do
+      before do
+        authenticate_platform!
+        authenticate_sn_code!
+      end
+
+      add_desc '密码找回'
+
+      params do
+        requires :platform, type: String, desc: '调用的平台(app或者erp)'
+        requires :phone_number, type: String, desc: '手机号'
+      end
+
+      get do
+        staff = current_store.store_staff.find_by(phone_number: params[:phone_number])
+        captcha = Captcha.create!(token: generate_salt(6), sent_at: Time.now, phone: params[:phone_number])
+        options = {
+          store_id: current_store.id,
+          receiver_type: "StoreStaff",
+          receiver_id: staff.id,
+          phone_number: params[:phone_number],
+          first_category: 'SmsCaptchaSwitchType',
+          second_category: SmsCaptchaSwitchType::TYPES_ID["密码找回验证"],
+          content: "尊敬的用户：您正在重置密码，验证码：#{captcha.token}，请在15分钟内按提示进行操作，切勿将验证码泄漏。 #{current_store.name}汽车服务门店"
+        }
+        SmsJob.perform_now(options)
+      end
+    end
+
+  end
+end
--- app/jobs/sms_job.rb
+++ app/jobs/sms_job.rb
@@ -2,21 +2,24 @@ class SmsJob < ActiveJob::Base
   queue_as :default
 
   def perform(options)
-    store = Store.find(options[:store_id])
+    store = Store.find_by_id(options[:store_id])
     if store.sms_balance.present? && (store.sms_balance.remaining > 0)
-      customer = StoreCustomer.find(options[:customer_id])
+      receiver = options[:receiver_type].constantize.find(options[:receiver_id])
       quantity = (options[:content].size / 70).ceil + 1
       store.sms_records.create!({
-        phone_number: customer.phone_number,
-        customer_name: customer.full_name,
-        customer_id: customer.id,
+        phone_number: receiver.phone_number,
+        receiver_name: receiver.full_name,
+        receiver_id: receiver.id,
         content: options[:content],
         first_category: options[:first_category],
         second_category: options[:second_category],
         quantity: quantity
       })
-      SmsClient.publish(customer.phone_number, options[:content])
+      SmsClient.publish(receiver.phone_number, options[:content])
       store.sms_balance.increase_sent_quantity!(quantity)
+      "sending message successfully"
+    else
+      "Error: There is no capability of sending message"
     end
   end
 end
