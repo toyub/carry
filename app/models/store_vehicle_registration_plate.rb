a03743151f76e9bcfc831ffa0e127323946a0e64
radiocontroller <zsh302643159@163.com>
2016-02-23 16:02:57 +0800

    add scope for uniqueness

--- app/models/store_vehicle_registration_plate.rb
+++ app/models/store_vehicle_registration_plate.rb
@@ -9,6 +9,7 @@ class StoreVehicleRegistrationPlate < ActiveRecord::Base
   has_many :vehicle_plates
   has_many :store_vehicles, through: :vehicle_plates
 
-  validates :license_number, presence: true, uniqueness: true, format: { with: /[\u4e00-\u9fa5]{1}[A-Z]{1}[A-Z_0-9]{5}/ }
+  validates :license_number, presence: true, uniqueness: { scope: :store_id }
+  validates :license_number, format: { with: /[\u4e00-\u9fa5]{1}[A-Z]{1}[A-Z_0-9]{5}/ }
 
 end
