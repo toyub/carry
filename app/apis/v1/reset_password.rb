748e01050c4514bcfacecad4312d4b59599b3418
tanyubin <tanyubin@icar99.com>
2016-02-24 11:10:58 +0800

    重置密码加入判断验证条件

--- app/apis/v1/reset_password.rb
+++ app/apis/v1/reset_password.rb
@@ -12,15 +12,17 @@ module V1
       params do
         requires :platform, type: String, desc: '调用的平台(app或者erp)'
         requires :phone_number, type: String, desc: '手机号'
+        requires :captcha, type: String, desc: '验证码'
         requires :new_password, type: String, desc: '新密码'
         requires :pass_confirmation, type: String, desc: '新密码确认'
       end
 
       post do
+        return {status: 'failed', notice: '输入验证码有误'} unless Captcha.authenticate(params[:phone_number], params[:captcha])
         staff = StoreStaff.find_by(phone_number: params[:phone_number])
-        return {status: 'failed', notice: 'user is not exist or password do unmatch'} if staff.blank? || (params[:new_password] != params[:pass_confirmation])
+        return {status: 'failed', notice: '用户不存在或密码不匹配'} if staff.blank? || (params[:new_password] != params[:pass_confirmation])
         staff.reset_password!(params[:new_password], params[:pass_confirmation])
-        {status: 'success', notice: 'reset password successfully'}
+        {status: 'success', notice: '重置成功'}
       end
     end
 
