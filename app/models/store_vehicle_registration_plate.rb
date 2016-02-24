2cc8dfe442524e4dbd4f55cfd98ae3482975fe12
radiocontroller <zsh302643159@163.com>
2016-02-24 10:19:57 +0800

    add format and length validation for vehicle plate

--- app/models/store_vehicle_registration_plate.rb
+++ app/models/store_vehicle_registration_plate.rb
@@ -9,6 +9,7 @@ class StoreVehicleRegistrationPlate < ActiveRecord::Base
   has_many :vehicle_plates
   has_many :store_vehicles, through: :vehicle_plates
 
-  validates :license_number, presence: true, uniqueness: { scope: :store_id }
+  validates :license_number, presence: true, uniqueness: { scope: :store_id }, length: { in: 7..8 }
+  validates :license_number, format: { with: /[\u4e00-\u9fa5_A-Za-z\d]/ }
 
 end
