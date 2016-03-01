419c505a86a3812fb3a67806d811a3ab818c72a2
朱士松 <zhushisong@icar99.com>
2016-03-01 13:14:31 +0800

    package itemable

--- app/apis/entities/package.rb
+++ app/apis/entities/package.rb
@@ -1,12 +1,21 @@
 module Entities
+  class StorePackageItem < Grape::Entity
+    expose(:store_package_item_id){|model| model.id}
+    expose(:retail_price) {|model| model.package_itemable.retail_price}
+    expose(:service_name) {|model| model.package_itemable.name}
+    expose(:work_time) {|model| model.package_itemable.time}
+    expose(:package_itemable_type) {|model|model.package_itemable_type}
+    expose(:package_itemable_id) {|model|model.package_itemable_id}
+  end
+
   class Package < Grape::Entity
-    expose :name
-    expose :store_name
-    expose :code
-    expose :valid_date
-    expose :retail_price
-    expose :point
-    expose :sold
-    expose :abstract
+    expose :name, :store_name, :code, :valid_date, :retail_price, :point, :sold, :abstract
+    expose(:store_package_id) {|model|model.id}
+
+    expose :package_services,using: StorePackageItem
+
+    def package_services
+      object.package_setting.services
+    end
   end
 end
