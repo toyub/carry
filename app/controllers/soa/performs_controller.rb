40937f44b614c0a3d38dbacbeb887718ccd436f9
tanyubin <tanyubin@icar99.com>
2016-03-07 15:13:35 +0800

    修改绩效提成查询功能

--- app/controllers/soa/performs_controller.rb
+++ app/controllers/soa/performs_controller.rb
@@ -15,8 +15,8 @@ class Soa::PerformsController < Soa::BaseController
       @date = Date.new params["date(1i)"].to_i, params["date(2i)"].to_i, params["date(3i)"].to_i
     end
     @staff = current_store.store_staff.find(params[:staff_id])
-    @order_items = @staff.store_order_items.where.not(orderable_type: StoreMaterialSaleinfoService.name).joins(:store_order).by_month(@date)
-    if @staff.mechanic?
+    @order_items = (@staff.store_order_items.where.not(orderable_type: StoreMaterialSaleinfoService.name).joins(:store_order).by_month(@date) unless params[:category] == 'constructed') || []
+    if @staff.mechanic? && params[:category] != 'sale'
       items = current_store.store_order_items.joins(:store_staff_tasks).where(store_staff_tasks: {store_staff_id: @staff.id}).by_month(@date)
       @order_items += items if items.present?
       @order_items.uniq!
