28911aa62c8938e3363d48432aa9fcad0167a40b
姜河 <jianghe@icar99.com>
2016-03-08 10:47:27 +0800

    Kucun: 修改入库记录显示

--- app/models/store_material_trans_receipt_item.rb
+++ app/models/store_material_trans_receipt_item.rb
@@ -7,6 +7,7 @@ class StoreMaterialTransReceiptItem < ActiveRecord::Base
   belongs_to :store_material_receipt
   belongs_to :store_material
   belongs_to :store_staff
+  belongs_to :receipt, class_name: 'StoreMaterialTransReceipt', foreign_key: 'store_material_receipt_id'
 
   def amount
     self.quantity * self.ordered_cost_price
