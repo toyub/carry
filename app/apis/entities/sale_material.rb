726d80866e5a9bd65b21c2074ec2e3f8fde9fd16
朱士松 <zhushisong@icar99.com>
2016-03-10 14:30:33 +0800

    refact material

--- app/apis/entities/sale_material.rb
+++ app/apis/entities/sale_material.rb
@@ -1,28 +1,15 @@
 module Entities
-  class StoreMaterialSaleinfoService < Grape::Entity
-    expose :name, :work_time, :quantity, :mechanic_level, :store_material_id,
-           :store_material_saleinfo_id, :mechanic_level_type
-    expose(:material_service_id){|model|model.id}
-  end
-
-  class MaterialPhotoPath < Grape::Entity
-    expose :file_url
-  end
-
+  included Material
   class SaleMaterial < Grape::Entity
     expose :id, :name, :barcode, :speci, :bargainable, :point,
            :retail_price, :bargain_price
     expose(:store_name) {|model| model.store.name}
-
     expose(:unit) {|model| model.store_material.store_material_unit.try :name}
-
     expose(:inventory) {|model|model.store_material.inventory}
     expose(:sold_count) {|model|model.store_material.sold_count}
     expose(:category) {|model|model.try(:category).try(:name)}
     expose :photo_path, using: MaterialPhotoPath
-
     expose :services, using: StoreMaterialSaleinfoService
-
 
     def photo_path
       object.store_material.uploads
