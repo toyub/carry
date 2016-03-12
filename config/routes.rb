6f0ddb7d670dacccdd446916b48dd9a6ececb258
issac <issaclau3@gmail.com>
2016-03-12 14:21:43 +0800

    为sidekiq添加验证

--- config/routes.rb
+++ config/routes.rb
@@ -496,6 +496,9 @@ Rails.application.routes.draw do
   end
   root 'kucun/materials#index'
 
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end if Rails.env.production?
   mount Sidekiq::Web => '/sidekiq'
 
 end
