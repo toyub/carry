a87c0dc3709160055dffb55385665a43215c06b0
朱士松 <zhushisong@icar99.com>
2016-03-17 11:54:52 +0800

    add vip-price in material

--- app/apis/entities/sale_material.rb
+++ app/apis/entities/sale_material.rb
@@ -1,7 +1,8 @@
 module Entities
   class SaleMaterial < Grape::Entity
     expose :id, :name, :barcode, :speci, :bargainable, :point,
-           :retail_price, :bargain_price, :sold_count
+           :retail_price, :bargain_price, :sold_count, :vip_price,
+           :vip_price_enabled
     expose(:store_name) {|model| model.store.name}
     expose(:unit) {|model| model.store_material.store_material_unit.try :name}
     expose(:inventory) {|model|model.store_material.inventory}
