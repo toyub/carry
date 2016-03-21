638afcb51fd426d492f51651011cdbaa7280f34f
radiocontroller <zsh302643159@163.com>
2016-03-21 15:57:58 +0800

    消费记录修复查询功能以及保留查询结果

--- app/controllers/crm/expense_records_controller.rb
+++ app/controllers/crm/expense_records_controller.rb
@@ -1,24 +1,38 @@
 class Crm::ExpenseRecordsController < Crm::BaseController
-  before_action :set_customer, :enmu, :set_search_params, only: [:index]
+
+  before_action :set_customer, :enmu, :set_search_params, :set_vehicles
+
   def index
     @q = @customer.orders.ransack(params[:q])
     @orders = @q.result.order(id: 'desc').includes(:store_vehicle)
   end
 
   private
-  def set_search_params
-    params[:q] = {created_at_lteq: ""} unless params[:q]
-    if params[:q][:created_at_lteq].present?
-      params[:q][:created_at_lteq] = params[:q][:created_at_lteq].to_time.end_of_day
+
+    def set_search_params
+      params[:q] ||= {}
+      get_search_params
+      params[:q][:created_at_gteq] = Time.zone.parse(params[:q][:created_at_gteq]).beginning_of_day if params[:q][:created_at_gteq].present?
+      params[:q][:created_at_lteq] = Time.zone.parse(params[:q][:created_at_lteq]).end_of_day if params[:q][:created_at_lteq].present?
     end
-  end
 
-  def enmu
-    @complaint_categories = Complaint::CATEGORY
-    @complaint_ways = Complaint::WAY
-  end
+    def enmu
+      @complaint_categories = Complaint::CATEGORY
+      @complaint_ways = Complaint::WAY
+    end
 
-  def set_customer
-    @customer = StoreCustomer.find(params[:store_customer_id])
-  end
+    def set_customer
+      @customer = StoreCustomer.find(params[:store_customer_id])
+    end
+
+    def set_vehicles
+      @vehicles = @customer.store_vehicles.map { |vehicle| [vehicle.license_number, vehicle.id] }
+    end
+
+    def get_search_params
+      @vehicle_id = params[:q][:store_vehicle_id_eq]
+      @beginning = params[:q][:created_at_gteq]
+      @end = params[:q][:created_at_lteq]
+    end
+
 end
