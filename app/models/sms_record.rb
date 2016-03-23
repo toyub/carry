7535af9b7b52ce54678a91b6472f042d7206e8ef
tanyubin <tanyubin@icar99.com>
2016-03-23 14:02:52 +0800

    修改issue #216 短信记录－类别显示不匹配bug

--- app/models/sms_record.rb
+++ app/models/sms_record.rb
@@ -5,4 +5,13 @@ class SmsRecord < ActiveRecord::Base
   scope :by_date, -> (start_date, end_date){ where(created_at: start_date .. end_date) }
   scope :by_category, ->(first_category, second_category) { where(first_category: first_category).where(second_category: second_category) }
   belongs_to :party, polymorphic: true
+
+  FIRST_CATEGORY_CN_NAME = {
+    'SaptchaSwitchType': "验证",
+    'SmsNotifySwitchType': "提醒",
+    'SmsTrackingSwitchType': "回访"
+  }
+  def category_cn_name
+    FIRST_CATEGORY_CN_NAME[first_category.to_sym].to_s + first_category.constantize.find(second_category).try(:name)
+  end
 end
