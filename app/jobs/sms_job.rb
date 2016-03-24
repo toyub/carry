068d3416572dd7c5698b30cbbde49697fe0e8e69
tanyubin <tanyubin@icar99.com>
2016-03-24 12:58:22 +0800

    添加receiver_type参数验证

--- app/jobs/sms_job.rb
+++ app/jobs/sms_job.rb
@@ -3,8 +3,12 @@ class SmsJob < ActiveJob::Base
 
   def perform(options)
     store = Store.find_by_id(options[:store_id])
-    if store.sms_balance.present? && (store.sms_balance.remaining > 0)
+    if options[:receiver_type].present? && (%w(StoreCustomer StoreStaff).include? options[:receiver_type])
       receiver = options[:receiver_type].constantize.find(options[:receiver_id])
+    else
+      return {success: false, notice: "Error: Can't find receiver"}
+    end
+    if store.present? && store.sms_balance.present? && (store.sms_balance.remaining > 0)
       quantity = (options[:content].size / 70).ceil + 1
       store.sms_records.create!({
         phone_number: receiver.phone_number,
