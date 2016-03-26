13bd340624d069b6d91293a3593fdf23468f3e6b
tanyubin <tanyubin@icar99.com>
2016-03-26 10:07:16 +0800

    developing

--- app/controllers/api/crm/customers_controller.rb
+++ app/controllers/api/crm/customers_controller.rb
@@ -20,14 +20,16 @@ module Api
       def check_vehicle_info
         return Status.new(success: true, notice: '车辆已存在!') if current_store.store_vehicle_registration_plates.find_by(license_number: params[:vehicle][:license_number])
         customer = current_store.store_customers.find_by(phone_number: params[:vehicle][:phone_number])
-        unless customer.present?
+        if customer.present?
+          customer.update!(customer_params)
+        else
           customer = current_store.store_customers.create!(customer_params)
           entity = customer.create_store_customer_entity(customer_relative_params)
           entity.create_store_customer_settlement(customer_relative_params)
         end
         vehicle = customer.store_vehicles.create!(vehicle_params)
         plate = vehicle.plates.create!(plate_params)
-        Status.new(success: true, notice: "添加#{plate.license_number}成功!")
+        Status.new(success: true, notice: "添加#{plate.license_number}成功!", license_number: params[:vehicle][:license_number])
       end
 
       def customer_params
--- app/services/status_object.rb
+++ app/services/status_object.rb
@@ -1,11 +1,12 @@
 module StatusObject
   class Status
-    attr_reader :notice, :customer, :success
+    attr_reader :notice, :customer, :success, :license_number
 
     def initialize(options = {})
       @success = options[:success]
       @notice = options[:notice]
       @customer = options[:customer]
+      @license_number = options[:license_number]
     end
 
     def success?
