3d66504b341c3996e000061bbb7f58c9debc5a96
朱士松 <zhushisong@icar99.com>
2016-03-28 11:26:28 +0800

    refact

--- app/apis/entities/vehicle_brand.rb
+++ app/apis/entities/vehicle_brand.rb
@@ -1,12 +1,9 @@
 module Entities
-  class Manufacturer < Grape::Entity
-    expose :name, :id
-  end
-
   class VehicleBrand < Grape::Entity
     expose :name
     expose :id
-    expose :manufacturer_series, using: Manufacturer, if: {type: :manufacturer}
+    expose :manufacturer_series, if: {type: :manufacturer}
+    
     private
     def manufacturer_series
       object.vehicle_series
