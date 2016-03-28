8a4d5979258a19a56ee981621c50c175cf2d47ff
朱士松 <zhushisong@icar99.com>
2016-03-28 11:16:31 +0800

    manufacturers

--- app/apis/entities/vehicle_brand.rb
+++ app/apis/entities/vehicle_brand.rb
@@ -1,6 +1,15 @@
 module Entities
+  class Manufacturer < Grape::Entity
+    expose :name, :id
+  end
+
   class VehicleBrand < Grape::Entity
     expose :name
     expose :id
+    expose :manufacturer_series, if: {type: :manufacturer}
+    private
+    def manufacturer_series
+      object.vehicle_series
+    end
   end
 end
