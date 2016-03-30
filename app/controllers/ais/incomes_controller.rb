1b7771ac2505a747a1b32653f7500a6a0a29cb52
tanyubin <tanyubin@icar99.com>
2016-03-30 10:06:28 +0800

    ais: data of income from current_store

--- app/controllers/ais/incomes_controller.rb
+++ app/controllers/ais/incomes_controller.rb
@@ -6,7 +6,6 @@ class Ais::IncomesController < Ais::BaseController
 
   private
   def get_categories
-
     @date = Date.today
     if params["date(1i)"] && params["date(2i)"]
       @date = Date.new params["date(1i)"].to_i, params["date(2i)"].to_i, params["date(3i)"].to_i
