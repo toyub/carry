cc4352dd6671dc6835688ea0d632586be4d13089
issac <issaclau3@gmail.com>
2016-03-31 16:17:49 +0800

    fixbug: 显示全部工位与技师

--- app/models/store_service_workflow_snapshot.rb
+++ app/models/store_service_workflow_snapshot.rb
@@ -21,21 +21,13 @@ class StoreServiceWorkflowSnapshot < ActiveRecord::Base
 
   def ready_mechanics(workstation_id)
     workstation = StoreWorkstation.find(workstation_id)
-    workstation.store_group.members.select {|m| m.store_group_member.ready?} if workstation
+    workstation.store_group.members if workstation
   end
 
   def workstations
     stations = StoreWorkstation.where(id: self.workstaiton_ids)
     return stations if stations.present?
     store.workstations
-  end
-
-  def free_workstations
-    self.workstations.idle
-  end
-
-  def screen_workstations
-    self.store_order.task_queuing? ? free_workstations : workstations
   end
 
   def workstaiton_ids
