5c5e751b69243304e7443bd5e517aabbe2b5a431
朱士松 <zhushisong@icar99.com>
2016-04-19 18:13:23 +0800

    fix

--- app/apis/v1/mechanics.rb
+++ app/apis/v1/mechanics.rb
@@ -1,8 +1,8 @@
 module V1
   class Mechanics < Grape::API
     before do
-      # authenticate_platform!
-      # authenticate_user!
+      authenticate_platform!
+      authenticate_user!
     end
 
     resource :mechanic do
@@ -12,7 +12,6 @@ module V1
           requires :platform, type: String, desc: '调用的平台!'
         end
         get  do
-          current_user = StoreStaff.find(11)
           workflowing = current_user.store_staff_tasks.current_task
           has_workflow = current_user.store_staff_tasks.have_task
           if workflowing.present?
