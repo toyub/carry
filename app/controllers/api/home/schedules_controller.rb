06b1bc3d174e59accaeedb3c11a734dc9c913213
tanyubin <tanyubin@icar99.com>
2016-04-29 15:15:14 +0800

    add calendar schedule js

--- app/controllers/api/home/schedules_controller.rb
+++ app/controllers/api/home/schedules_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module Home
     class SchedulesController < BaseController
+      before_action :reset_date, only: :create
+
       def index
         @schedules = current_user.schedules
       end
@@ -13,6 +15,11 @@ module Api
       private
       def schedule_param
         params.require(:schedule).permit(:title, :start_time, :end_time, :remark)
+      end
+
+      def reset_date
+        params[:schedule][:start_time] = DateTime.parse(params[:schedule_date] + ' ' + params[:schedule][:start_time]) if params[:schedule][:start_time].present?
+        params[:schedule][:end_time] = DateTime.parse(params[:schedule_date] + ' ' + params[:schedule][:end_time]) if params[:schedule][:end_time].present?
       end
     end
   end
