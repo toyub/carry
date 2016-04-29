3e801f989b969a8c38edca1d9c32066272b8e943
朱士松 <zhushisong@icar99.com>
2016-04-29 17:34:27 +0800

    data

--- db/schema.rb
+++ db/schema.rb
@@ -885,9 +885,9 @@ ActiveRecord::Schema.define(version: 201604198905987) do
     t.integer  "received_status",                                              default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chain_business_model_id",                                      default: 0,   null: false, comment: "门店加入连锁时选择的商业模式，目前有连锁模式和加盟模式，默认是连锁模式（0）"
     t.integer  "withdrawaler_id"
     t.datetime "withdrawal_at"
-    t.integer  "chain_business_model_id",                                      default: 0,   null: false, comment: "门店加入连锁时选择的商业模式，目前有连锁模式和加盟模式，默认是连锁模式（0）"
   end
 
   create_table "store_material_outing_items", force: :cascade do |t|
@@ -1716,20 +1716,20 @@ ActiveRecord::Schema.define(version: 201604198905987) do
     t.string   "reason_for_leave"
     t.string   "numero"
     t.integer  "store_position_id"
+    t.json     "bonus",                                                       default: {}
+    t.decimal  "trial_salary",                       precision: 10, scale: 2
+    t.decimal  "regular_salary",                     precision: 10, scale: 2
+    t.decimal  "previous_salary",                    precision: 10, scale: 2
+    t.integer  "trial_period"
     t.integer  "store_employee_id"
+    t.json     "skills",                                                      default: {}
+    t.json     "other",                                                       default: {}
     t.string   "full_name"
     t.string   "phone_number"
     t.boolean  "mis_login_enabled",                                           default: false
     t.boolean  "app_login_enabled",                                           default: false
     t.boolean  "erp_login_enabled",                                           default: false
     t.integer  "roles",                                                                                             array: true
-    t.json     "bonus",                                                       default: {}
-    t.decimal  "trial_salary",                       precision: 10, scale: 2
-    t.decimal  "regular_salary",                     precision: 10, scale: 2
-    t.decimal  "previous_salary",                    precision: 10, scale: 2
-    t.integer  "trial_period"
-    t.json     "skills",                                                      default: {}
-    t.json     "other",                                                       default: {}
     t.boolean  "deduct_enabled",                                              default: false
     t.integer  "deadline_days"
     t.boolean  "contract_notice_enabled",                                     default: false
