d91aec4673be153b1608a7fb6dc5c91558232a15
朱士松 <zhushisong@icar99.com>
2016-04-29 13:55:22 +0800

    refact

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   layout "advancev1"
   before_filter :login_required
-  before_action :get_works, only: [:show]
+  before_action :get_works, :my_works, only: [:show]
 
   def show
     @orders = current_store.today_orders
@@ -16,5 +16,18 @@ class HomeController < ApplicationController
   private
   def get_works
     @works = (YAML.load_file Rails.root.join("config", "my_work.yml")).with_indifferent_access[:works]
+  end
+
+  def my_works
+    get_works
+    @my_works = []
+    if current_user.works.present?
+      @works.each do |root_category|
+        root_category[:sub_categories].each do |work|
+          @my_works << work if current_user.works.include?(work[:idx].to_s)
+        end
+      end
+    end
+    @my_works
   end
 end
