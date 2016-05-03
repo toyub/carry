e0966bad5756185e7b5f28ba8870c61ebf3b7cbc
姜河 <jianghe@icar99.com>
2016-05-03 15:21:34 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -559,7 +559,7 @@ ActiveRecord::Schema.define(version: 201604198905987) do
     t.datetime "updated_at",                                                      null: false
     t.integer  "store_customer_entity_id"
     t.decimal  "credit_bill_amount",       precision: 12, scale: 2, default: 0.0, null: false
-    t.decimal  "credit_limit",             precision: 12, scale: 2, default: 0.0, null: false
+    t.decimal  "credit_limit",             precision: 12, scale: 2, default: 0.0
     t.integer  "credit",                                            default: 0
     t.integer  "notice_period",                                     default: 0
     t.integer  "payment_mode",                                      default: 0
@@ -973,7 +973,7 @@ ActiveRecord::Schema.define(version: 201604198905987) do
     t.integer  "quantity",                                                                       null: false
     t.decimal  "cost_price",                              precision: 12, scale: 2, default: 0.0, null: false
     t.decimal  "amount",                                  precision: 14, scale: 4
-    t.decimal  "inventory_cost_price",                    precision: 12, scale: 2, default: 0.0, null: false
+    t.decimal  "inventory_cost_price",                    precision: 12, scale: 2, default: 0.0
     t.string   "remark",                      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -1089,8 +1089,8 @@ ActiveRecord::Schema.define(version: 201604198905987) do
     t.integer  "store_staff_id",                                                          null: false
     t.integer  "store_material_id",                                                       null: false
     t.boolean  "bargainable",                                             default: false
-    t.decimal  "bargain_price",                  precision: 12, scale: 2, default: 0.0,   null: false
-    t.decimal  "retail_price",                   precision: 12, scale: 2, default: 0.0,   null: false
+    t.decimal  "bargain_price",                  precision: 12, scale: 2, default: 0.0
+    t.decimal  "retail_price",                   precision: 12, scale: 2, default: 0.0
     t.decimal  "trade_price",                    precision: 12, scale: 2, default: 0.0
     t.integer  "reward_points",                                           default: 0
     t.boolean  "divide_to_retail",                                        default: false
